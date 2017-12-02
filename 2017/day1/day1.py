97ed3c5708e3855d50ee1fd5ec2364b3837797c3
Ed Rogers <etr@orc.soton.ac.uk>
2017-12-02 12:06:09 +0000

    2017 Day 1 part 2

--- 2017/day1/day1.py
+++ 2017/day1/day1.py
@@ -6,13 +6,21 @@ Created on Sat Dec  2 08:36:23 2017
 @author: user
 """
 
-def captcha(digits):
+def captcha(digits,step=1):
+    
+        
     if isinstance(digits, str):
         digits = [int(c) for c in digits if c.isdigit()]
         # ignore non-digit characters
+    
+    #must do after conversion as newline breaks the length
+    if step is None:
+        step = int(len(digits)/2)
+        print(step)
+    
+    shifted = digits[step:]
+    shifted.extend(digits[0:step])
     print(digits)
-    shifted = digits[1:]
-    shifted.append(digits[0])
     print(shifted)
     matches = [t[0] for t in zip(digits, shifted) if t[0]==t[1]]
     return sum(matches)
@@ -21,7 +29,8 @@ def main():
     #read file
     with open('input.txt', 'r') as file:
         digits = file.read()
-    out = captcha(digits)
+    #digits = '1111'
+    out = captcha(digits,None)
     print(out)
     
 if __name__ == '__main__':
--- 2017/day1/test_day1a.py
+++ 2017/day1/test_day1a.py
@@ -12,3 +12,22 @@ def test3():
 
 def test4():
 	assert day1.captcha('91212129') == 9
+    
+
+# PART 2
+def test5():
+	assert day1.captcha('1212',None) == 6
+
+def test6():
+	assert day1.captcha('1221',None) == 0
+
+def test7():
+	assert day1.captcha('123425',None) == 4
+
+def test8():
+	assert day1.captcha('123123',None) == 12
+
+def test9():
+	assert day1.captcha('12131415',None) == 4
+    
+    
