a73312662af64792b8ff913e0ef68709aa3af5cb
Ed Rogers <etr@orc.soton.ac.uk>
2017-12-05 13:33:15 +0000

    Remove old commented out code from day2_tests

--- 2017/day2/test_day2.py
+++ 2017/day2/test_day2.py
@@ -19,30 +19,5 @@ def test2():
             [3, 8, 6, 5]]
     assert day2.checksum(data, True) == 9
 
-#def test2():
-#	assert day1.captcha('1111') == 4
-#
-#def test3():
-#	assert day1.captcha('1234') == 0
-#
-#def test4():
-#	assert day1.captcha('91212129') == 9
-#    
-#
-## PART 2
-#def test5():
-#	assert day1.captcha('1212',None) == 6
-#
-#def test6():
-#	assert day1.captcha('1221',None) == 0
-#
-#def test7():
-#	assert day1.captcha('123425',None) == 4
-#
-#def test8():
-#	assert day1.captcha('123123',None) == 12
-#
-#def test9():
-#	assert day1.captcha('12131415',None) == 4
-    
-    
+
+
