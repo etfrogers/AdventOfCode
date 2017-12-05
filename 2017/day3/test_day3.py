27cd93067bbecb55dfd9b9637133f25e49f1c9f3
Ed Rogers <etr@orc.soton.ac.uk>
2017-12-05 13:34:37 +0000

    Add day3 tests

---
+++ 2017/day3/test_day3.py
@@ -0,0 +1,30 @@
+# from nose.tools import *
+import day3
+
+
+def test_manhattan_dist1():
+    assert day3.manhattan_dist((0, 0), (0, 0)) == 0
+
+
+def test_manhattan_dist2():
+    assert day3.manhattan_dist((2, 1), (0, 0)) == 3
+
+
+def test_manhattan_dist3():
+    assert day3.manhattan_dist((0, -2), (0, 0)) == 2
+
+
+def test_spiral_distance1():
+    assert day3.spiral_distance(1) == 0
+
+
+def test_spiral_distance2():
+    assert day3.spiral_distance(12) == 3
+
+
+def test_spiral_distance3():
+    assert day3.spiral_distance(23) == 2
+
+
+def test_spiral_distance4():
+    assert day3.spiral_distance(1024) == 31
