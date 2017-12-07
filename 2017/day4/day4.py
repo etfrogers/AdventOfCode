8573081ca67e675f79e0db2bb916e6327c51d9ef
Ed Rogers <etr@orc.soton.ac.uk>
2017-12-07 11:32:48 +0000

    2017 Day 4 part 2

--- 2017/day4/day4.py
+++ 2017/day4/day4.py
@@ -1,14 +1,16 @@
 
-def is_valid(passphrase):
+def is_valid(passphrase, with_anagrams=False):
     token_list = passphrase.split(' ')
+    if with_anagrams:
+        token_list = [''.join(sorted(token)) for token in token_list]
     token_set = set(token_list)
     print(passphrase)
     print('%d tokens found, %d unique' % (len(token_list), len(token_set)))
     return len(token_list) == len(token_set)
 
 
-def count_valid_phrases(phraselist):
-    valid_list = [is_valid(phrase) for phrase in phraselist]
+def count_valid_phrases(phraselist, with_anagrams=False):
+    valid_list = [is_valid(phrase, with_anagrams) for phrase in phraselist]
     return sum(valid_list)
 
 
@@ -16,7 +18,7 @@ def main():
     with open('input.txt', 'r') as file:
         phrase_list = file.readlines()
     phrase_list = [line.strip() for line in phrase_list]
-    count = count_valid_phrases(phrase_list)
+    count = count_valid_phrases(phrase_list, True)
     print('%d of %d are valid' % (count, len(phrase_list)))
 
 
--- 2017/day4/day4_tests.py
+++ 2017/day4/day4_tests.py
@@ -19,3 +19,31 @@ def test_part_1():
     phrase_list = [line.strip() for line in phrase_list]
     count = day4.count_valid_phrases(phrase_list)
     assert count == 386
+
+
+def test_is_valid_ana1():
+    assert day4.is_valid('abcde fghij', True)
+
+
+def test_is_valid_ana2():
+    assert not day4.is_valid('abcde xyz ecdab', True)
+
+
+def test_is_valid_ana3():
+    assert day4.is_valid('a ab abc abd abf abj', True)
+
+
+def test_is_valid_ana4():
+    assert day4.is_valid('iiii oiii ooii oooi oooo', True)
+
+
+def test_is_valid_ana5():
+    assert not day4.is_valid('oiii ioii iioi iiio', True)
+
+
+def test_part_2():
+    with open('input.txt', 'r') as file:
+        phrase_list = file.readlines()
+    phrase_list = [line.strip() for line in phrase_list]
+    count = day4.count_valid_phrases(phrase_list, True)
+    assert count == 208
