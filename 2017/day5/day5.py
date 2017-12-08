33227e1f1782144f2abe0b42237c8b40a7543c4e
Ed Rogers <etr@orc.soton.ac.uk>
2017-12-08 10:00:10 +0000

    2017 Day 5 part 1

---
+++ 2017/day5/day5.py
@@ -0,0 +1,36 @@
+
+def in_bounds(inst, pos):
+    return 0 <= pos < len(inst)
+
+
+def to_str(inst, pos):
+    out_list = [str(i) for i in inst]
+    out_list[pos] = '(' + out_list[pos] + ')'
+    return ' '.join(out_list)
+
+
+def count_jumps(inst, part2=False):
+    pos = 0
+    steps = 0
+    while in_bounds(inst, pos):
+        # print(to_str(inst, pos))
+        curr_inst = inst[pos]
+        inst[pos] += 1
+        pos = pos+curr_inst
+        steps += 1
+    return steps
+
+
+def main():
+    with open('input.txt', 'r') as file:
+        inst = file.readlines()
+    inst = [line.strip() for line in inst]
+    inst = [int(l) for l in inst]
+    # inst = [0, 3, 0, 1, -3]
+    print(inst)
+    count = count_jumps(inst)
+    print(count)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2017/day5/day5_tests.py
@@ -0,0 +1,15 @@
+import day5
+
+
+def test_count_jumps1():
+    assert day5.count_jumps([0, 3, 0, 1, -3]) == 5
+
+
+def test_count_jump_part1():
+    with open('input.txt', 'r') as file:
+        inst = file.readlines()
+    inst = [line.strip() for line in inst]
+    inst = [int(l) for l in inst]
+    assert day5.count_jumps(inst) == 336905
+
+
