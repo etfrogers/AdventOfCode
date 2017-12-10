e8198e22a6efcbc2fa043ee5cdff1eff131b6158
Ed Rogers <etr@orc.soton.ac.uk>
2017-12-10 09:56:28 +0000

    2017 Day 6 part 2

--- 2017/day6/day6.py
+++ 2017/day6/day6.py
@@ -17,20 +17,22 @@ def balance_blocks(blocks):
         config_list.append(tuple(blocks))
         do_balancing_cycle(blocks)
         cycles += 1
-    return cycles, config_list
+    loop_length = len(config_list) - config_list.index(tuple(blocks))
+    return cycles, loop_length, config_list
 
 
 def main():
-    # with open('input.txt', 'r') as file:
+    # with open('puzzle_input.txt', 'r') as file:
     #     blocks = file.readlines()
     # blocks = [line.strip() for line in blocks]
     # blocks = [int(l) for l in blocks]
-    input = [4, 1, 15, 12, 0, 9, 9, 5, 5, 8, 7, 3, 14, 5, 12, 3]
+    puzzle_input = [4, 1, 15, 12, 0, 9, 9, 5, 5, 8, 7, 3, 14, 5, 12, 3]
     blocks = [0, 2, 7, 0]
-    blocks = input
+    blocks = puzzle_input
     print(blocks)
-    count, config_list = balance_blocks(blocks)
+    count, loop_length, config_list = balance_blocks(blocks)
     print(count)
+    print(loop_length)
 
 
 if __name__ == '__main__':
--- 2017/day6/day6_tests.py
+++ 2017/day6/day6_tests.py
@@ -1,12 +1,24 @@
 import day6
 
+# noinspection PyShadowingBuiltins
+input = [4, 1, 15, 12, 0, 9, 9, 5, 5, 8, 7, 3, 14, 5, 12, 3]
+
 
 def test_balance_blocks1():
-    count, _ = day6.balance_blocks([0, 2, 7, 0])
+    count, _, _ = day6.balance_blocks([0, 2, 7, 0])
     assert count == 5
 
 
 def test_part1():
-    input = [4, 1, 15, 12, 0, 9, 9, 5, 5, 8, 7, 3, 14, 5, 12, 3]
-    count, _ = day6.balance_blocks(input)
+    count, _, _ = day6.balance_blocks(input)
     assert count == 6681
+
+
+def test_balance_blocks2():
+    _, loop_length, _ = day6.balance_blocks([0, 2, 7, 0])
+    assert loop_length == 4
+
+
+def test_part2():
+    _, loop_length, _ = day6.balance_blocks(input)
+    assert loop_length == 2392
