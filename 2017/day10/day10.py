7d04cc419dc3e5c115e142e6643658051bd3ee93
Ed Rogers <etr@orc.soton.ac.uk>
2017-12-15 11:30:57 +0000

    2017 Day 10 part 2

--- 2017/day10/day10.py
+++ 2017/day10/day10.py
@@ -1,34 +1,69 @@
 import numpy as np
+import functools
 
 
-def do_knot_hash(n, lengths):
-    array = np.array(range(0, n))
+def do_knot_hash(hash, lengths, position=0, skip_size=0):
+    try:
+        iter(hash)
+    except TypeError:
+        hash = np.array(range(0, hash))
 
-    skip_size = 0
-    position = 0
-
+    n = len(hash)
     for length in lengths:
         if length != 0:
             indexes = (np.array(list(range(0, length))) + position) % n
-            array[indexes] = np.flipud(array[indexes])
+            hash[indexes] = np.flipud(hash[indexes])
         position = (position + length + skip_size) % n
         skip_size += 1
-    return array
+    return hash, position, skip_size
 
 
 def calc_output(results):
     return results[0] * results[1]
 
 
+def string_to_lengths(string):
+    lengths = [ord(c) for c in string]
+    return lengths
+
+
+def sparse_to_dense(hash):
+    hash = np.reshape(hash, (16, -1))
+    dense_hash = []
+    for line in hash:
+        dense_hash.append(functools.reduce((lambda a, b: a ^ b), line))
+    return dense_hash
+
+
+def hash_to_string(dense_hash):
+    list_of_chars_pairs = [hex(val)[2:] for val in dense_hash]
+    list_of_chars_pairs = ['0'+c if len(c) == 1 else c for c in list_of_chars_pairs]
+    return ''.join(list_of_chars_pairs)
+
+
+def full_hash(n, string):
+    lengths = string_to_lengths(string)
+    suffix = [17, 31, 73, 47, 23]
+    lengths = lengths + suffix
+
+    hash = np.array(range(0, n))
+    position = 0
+    skip_size = 0
+    for i in range(0,64):
+        hash, position, skip_size = do_knot_hash(hash, lengths, position, skip_size)
+
+    dense_hash = sparse_to_dense(hash)
+    str_hash = hash_to_string(dense_hash)
+    return str_hash
+
+
 def main():
-    lengths = np.loadtxt('input.txt', dtype=int)
-    n = 256
-    # lengths = [3, 4, 1, 5]
-    # n = 5
-    results = do_knot_hash(n, lengths)
-    print(results)
-    output = calc_output(results)
-    print(output)
+    with open('input.txt', 'r') as file:
+        lengths = file.read()
+    lengths = lengths.strip()
+
+    string = full_hash(256, lengths)
+    print(string)
 
 
 if __name__ == '__main__':
--- 2017/day10/day10_tests.py
+++ 2017/day10/day10_tests.py
@@ -5,15 +5,40 @@ import numpy as np
 def test_knot_hash():
     lengths = [3, 4, 1, 5]
     n = 5
-    results = day10.do_knot_hash(n, lengths)
+    results, _, _ = day10.do_knot_hash(n, lengths)
     assert tuple(results) == (3, 4, 2, 1, 0)
     output = day10.calc_output(results)
     assert output == 12
 
 
 def test_part1():
-    lengths = np.loadtxt('input.txt', dtype=int)
+    lengths = np.loadtxt('input.txt', dtype=int, delimiter=',')
     n = 256
-    results = day10.do_knot_hash(n, lengths)
+    results, _, _ = day10.do_knot_hash(n, lengths)
     output = day10.calc_output(results)
     assert output == 48705
+
+
+def test_full_hash1():
+    assert day10.full_hash(256, '') == 'a2582a3a0e66e6e86e3812dcb672a272'
+
+
+def test_full_hash2():
+    assert day10.full_hash(256, 'AoC 2017') == '33efeb34ea91902bb2f59c9920caa6cd'
+
+
+def test_full_hash3():
+    assert day10.full_hash(256, '1,2,3') == '3efbe78a8d82f29979031a4aa0b16a9d'
+
+
+def test_full_hash4():
+    assert day10.full_hash(256, '1,2,4') == '63960835bcdc130f0b66d7ff4f6a5a8e'
+
+
+def test_part2():
+    with open('input.txt', 'r') as file:
+        lengths = file.read()
+    lengths = lengths.strip()
+    string = day10.full_hash(256, lengths)
+    assert string == '1c46642b6f2bc21db2a2149d0aeeae5d'
+
