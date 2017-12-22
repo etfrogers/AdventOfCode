fe3d7e8d94d648da7ae650ed2313380521557cd0
Ed Rogers <etr@orc.soton.ac.uk>
2017-12-22 13:09:58 +0000

    2017 Day 15 part 2

--- 2017/day15/day15.py
+++ 2017/day15/day15.py
@@ -4,19 +4,30 @@ LOWEST_16_BITS = (2**16) - 1
 FACTOR_A = 16807
 FACTOR_B = 48271
 PART_1_ITERATIONS = 40000000
+PART_2_ITERATIONS = 5000000
+PICKY_VAL_A = 4
+PICKY_VAL_B = 8
 
 
 class Generator:
-    def __init__(self, factor, start_val):
+    def __init__(self, factor, start_val, picky_val=None):
         self.factor = factor
         self.last_val = start_val
+        self.is_picky = picky_val is not None
+        self.picky_val = picky_val
 
     def __iter__(self):
         return self
 
-    def __next__(self):
+    def gen_val(self):
         val = (self.last_val * self.factor) % DIVISOR
         self.last_val = val
+        return val
+
+    def __next__(self):
+        val = self.gen_val()
+        while self.is_picky and val % self.picky_val != 0:
+            val = self.gen_val()
         return val
 
 
@@ -24,26 +35,27 @@ def compare(int1, int2):
     return (int1 & LOWEST_16_BITS) == (int2 & LOWEST_16_BITS)
 
 
-def main():
-    start_a = 65    # test
-    start_b = 8921  # test
-    start_a = 679   # real
-    start_b = 771   # real
-
-    gen_a = Generator(FACTOR_A, start_a)
-    gen_b = Generator(FACTOR_B, start_b)
-    matches = count_matches(gen_a, gen_b, PART_1_ITERATIONS)
-    print(matches)
-
-
 def count_matches(gen_a, gen_b, n_iterations):
     matches = 0
     for i, valA, valB in zip(range(0, n_iterations), gen_a, gen_b):
         if (i % 100000) == 0:
             print(i)
+        # print((valA, valB))
         if compare(valA, valB):
             matches += 1
     return matches
+
+
+def main():
+    # start_a = 65    # test
+    # start_b = 8921  # test
+    start_a = 679   # real
+    start_b = 771   # real
+
+    gen_a = Generator(FACTOR_A, start_a, PICKY_VAL_A)
+    gen_b = Generator(FACTOR_B, start_b, PICKY_VAL_B)
+    matches = count_matches(gen_a, gen_b, PART_2_ITERATIONS)
+    print(matches)
 
 
 if __name__ == '__main__':
--- 2017/day15/day15_tests.py
+++ 2017/day15/day15_tests.py
@@ -9,6 +9,13 @@ gen_values_part1 = [[   1092455,  430625591],
                     [1744312007,  137874439],
                     [1352636452,  285222916]]
 
+#                       A             B
+gen_values_part2 = [[1352636452,  1233683848],
+                    [1992081072,   862516352],
+                    [530830436,   1159784568],
+                    [1980017072,  1616057672],
+                    [740335192,    412269392]]
+
 run_slow_tests = bool(testconfig.config.get('slow', False))
 
 
@@ -38,7 +45,7 @@ def test_gen_b_values_part1():
         assert row[1] == val
 
 
-def test_long_matches():
+def test_long_matches1():
     if not run_slow_tests:
         return True
 
@@ -62,3 +69,32 @@ def test_part1():
     gen_b = day15.Generator(day15.FACTOR_B, start_b)
     matches = day15.count_matches(gen_a, gen_b, day15.PART_1_ITERATIONS)
     assert matches == 626
+
+
+def test_gen_a_values_part2():
+    start_a = 65  # test
+
+    gen_a = day15.Generator(day15.FACTOR_A, start_a, day15.PICKY_VAL_A)
+    for row, val in zip(gen_values_part2, gen_a):
+        assert row[0] == val
+
+
+def test_gen_b_values_part2():
+    start_b = 8921  # test
+
+    gen_b = day15.Generator(day15.FACTOR_B, start_b, day15.PICKY_VAL_B)
+    for row, val in zip(gen_values_part2, gen_b):
+        assert row[1] == val
+
+
+def test_long_matches1():
+    if not run_slow_tests:
+        return True
+
+    start_a = 65  # test
+    start_b = 8921  # test
+
+    gen_a = day15.Generator(day15.FACTOR_A, start_a, day15.PICKY_VAL_A)
+    gen_b = day15.Generator(day15.FACTOR_B, start_b, day15.PICKY_VAL_B)
+    matches = day15.count_matches(gen_a, gen_b, day15.PART_2_ITERATIONS)
+    assert matches == 309
