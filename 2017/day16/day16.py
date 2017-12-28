f5074b0295f4185a5b577b8a21b53356e9d18081
Ed Rogers <etr@orc.soton.ac.uk>
2017-12-28 11:22:28 +0000

    2017 Day 16 part 2: non-working repositioning algorithm

--- 2017/day16/day16.py
+++ 2017/day16/day16.py
@@ -1,10 +1,27 @@
+from enum import Enum
+
+
+class Speed(Enum):
+    SLOW = 0
+    FASTER = 1
+    FASTEST = 2
+
 
 class Dancers:
-    def __init__(self, length):
-        self._formation = [chr(ord('a') + v) for v in range(0, length)]
+    def __init__(self, length, start_char='a'):
+        self._length = length
+        self._start_char = start_char
+        self._formation = self.char_list()
         self.move_dict = {'s': {'func': self.spin, 'nargs': 1, 'conv': int},
                           'x': {'func': self.exchange, 'nargs': 2, 'conv': int},
                           'p': {'func': self.partner, 'nargs': 2, 'conv': lambda x: x}}
+        self.reposition = None
+
+    def char_list(self):
+        return [chr(ord(self._start_char) + v) for v in range(0, self._length)]
+
+    def reset(self):
+        self._formation = self.char_list()
 
     @property
     def formation(self):
@@ -30,16 +47,76 @@ class Dancers:
     def perform(self, moves):
         [self.do_move(move) for move in moves]
 
+    def store_reposition(self):
+        self.reposition = [self.char_list().index(c) for c in self._formation]
+        print(self.reposition)
+
+    def apply_reposition(self):
+        if self.reposition is None:
+            raise AttributeError
+        else:
+            # self._formation = [self._formation[i] for i in self.reposition]
+            print('-------------------')
+            print('Applying reposition')
+            print('State before')
+            print(self.formation)
+            for j, i in enumerate(self.reposition):
+                print('%d - %d: %s' % (j, i, self._formation[i]))
+            self._formation = [self._formation[i] for i in self.reposition]
+            print('State after')
+            print(self.formation)
+            print('Finished reposition')
+            print('-------------------')
+
+    def whole_dance(self, moves, reps, speed=Speed.FASTEST):
+        self.reset()
+        if speed == speed.SLOW:
+            for _ in range(reps):
+                self.perform(moves)
+            return None
+
+        self.perform(moves)
+        self.store_reposition()
+        self.reset()
+
+        configs = []
+        for i in range(0, reps):
+            self.apply_reposition()
+            if speed == Speed.FASTEST:
+                if self.formation in configs:
+                    break
+                configs.append(self.formation)
+            # if i % 10000 == 0:
+        if speed == Speed.FASTER:
+            return None
+        else:  # speed = Speed.FASTEST
+            pre_len = configs.index(self.formation)
+            #print(pre_len)
+            loop_len = len(configs)-pre_len
+            remainder = (reps - pre_len) % loop_len
+
+            self._formation = configs[remainder]
+            # print(self.formation)
+
 
 def main():
-    with open('input.txt', 'r') as file:
+    reps = int(1e9)
+    reps = int(1e6)
+    with open('test_input.txt', 'r') as file:
         dance_moves = file.read()
     dance_moves = dance_moves.strip()
     dance_moves = dance_moves.split(',')
-    dancers = Dancers(16)
+    dancers = Dancers(5, 'a')
 
-    dancers.perform(dance_moves)
-    print(dancers.formation)
+    reps = 50
+    dancers.whole_dance(dance_moves, reps, Speed.SLOW)
+    slow_formation = dancers.formation
+    print(slow_formation)
+    dancers.store_reposition()
+    print(dancers.reposition)
+    dancers.whole_dance(dance_moves, reps, Speed.FASTER)
+    faster_formation = dancers.formation
+    print(faster_formation)
 
 
 if __name__ == '__main__':
--- 2017/day16/day16_tests.py
+++ 2017/day16/day16_tests.py
@@ -22,3 +22,111 @@ def test_part1():
     dancers.perform(dance_moves)
     assert dancers.formation == 'kgdchlfniambejop'
 
+
+def test_reposition():
+    with open('input.txt', 'r') as file:
+        dance_moves = file.read()
+    dance_moves = dance_moves.strip()
+    dance_moves = dance_moves.split(',')
+    dancers = day16.Dancers(16)
+
+    dancers.perform(dance_moves)
+    first_formation = dancers.formation
+    dancers.store_reposition()
+    dancers.reset()
+    dancers.apply_reposition()
+    second_formation = dancers.formation
+
+    assert first_formation == second_formation
+
+
+def test_reposition2():
+    with open('input.txt', 'r') as file:
+        dance_moves = file.read()
+    dance_moves = dance_moves.strip()
+    dance_moves = dance_moves.split(',')
+    dancers = day16.Dancers(16)
+
+    dancers.perform(dance_moves)
+    dancers.store_reposition()
+    dancers.perform(dance_moves)
+    first_formation = dancers.formation
+    dancers.reset()
+    dancers.apply_reposition()
+    dancers.apply_reposition()
+    second_formation = dancers.formation
+
+    assert first_formation == second_formation
+
+
+def test_reposition3():
+    with open('test_input.txt', 'r') as file:
+        dance_moves = file.read()
+    dance_moves = dance_moves.strip()
+    dance_moves = dance_moves.split(',')
+    dancers = day16.Dancers(5)
+
+    dancers.perform(dance_moves)
+    dancers.store_reposition()
+    first_formation = dancers.formation
+    dancers.reset()
+    dancers.apply_reposition()
+    second_formation = dancers.formation
+
+    assert first_formation == second_formation
+
+
+def test_reposition4():
+    with open('test_input.txt', 'r') as file:
+        dance_moves = file.read()
+    dance_moves = dance_moves.strip()
+    dance_moves = 's1,x3/4,s3,x1/4,x3/1,s4,pa/dx4/1'
+    dance_moves = dance_moves.split(',')
+    dancers = day16.Dancers(5)
+
+    print(dancers.formation)
+    dancers.perform(dance_moves)
+    print(dancers.formation)
+    dancers.store_reposition()
+    dancers.perform(dance_moves)
+    print(dancers.formation)
+    first_formation = dancers.formation
+    dancers.reset()
+    dancers.apply_reposition()
+    dancers.apply_reposition()
+    second_formation = dancers.formation
+
+    assert first_formation == second_formation
+
+
+def test_fast_whole_dance1():
+    with open('input.txt', 'r') as file:
+        dance_moves = file.read()
+    dance_moves = dance_moves.strip()
+    dance_moves = dance_moves.split(',')
+    dancers = day16.Dancers(16)
+
+    reps = 10
+    dancers.whole_dance(dance_moves, reps, day16.Speed.SLOW)
+    slow_formation = dancers.formation
+    dancers.whole_dance(dance_moves, reps, day16.Speed.FASTER)
+    faster_formation = dancers.formation
+
+    assert slow_formation == faster_formation
+
+
+def test_faster_whole_dance1():
+    with open('input.txt', 'r') as file:
+        dance_moves = file.read()
+    dance_moves = dance_moves.strip()
+    dance_moves = dance_moves.split(',')
+    dancers = day16.Dancers(16)
+
+    reps = 200
+    dancers.whole_dance(dance_moves, reps, day16.Speed.FASTER)
+    slow_formation = dancers.formation
+
+    dancers.whole_dance(dance_moves, reps, day16.Speed.FASTEST)
+    faster_formation = dancers.formation
+
+    assert slow_formation == faster_formation
