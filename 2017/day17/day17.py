31045f9655cd7e75ba5ad2ac28500409436f37ad
Ed Rogers <etr@orc.soton.ac.uk>
2017-12-28 16:11:39 +0000

    2017 Day 17 part 2

--- 2017/day17/day17.py
+++ 2017/day17/day17.py
@@ -4,24 +4,42 @@ def calc_spinlock(n, step):
     buffer = [0]
     done = False
     val_to_insert = 1
-    # print(buffer)
+    pos_list = []
     while not done:
         new_pos = (pos + step) % len(buffer)
-        # print(new_pos)
         buffer.insert(new_pos+1, val_to_insert)
         val_to_insert += 1
-        # print(buffer)
+        pos_list.append(pos)
         pos = new_pos + 1
         done = val_to_insert > n
+    print(pos_list)
     value = buffer[pos+1]
     return value, buffer
 
 
+def spinlock_track_0(n, step):
+    pos = 1
+    pos_0 = 0
+    val_after_0 = 1
+    length = 2
+    for val in range(2, n + 1):
+        pos = ((pos + step) % length) + 1
+        if pos == (pos_0 + 1) % length:
+            val_after_0 = val
+        if pos <= pos_0:
+            pos_0 += 1
+        length += 1
+        if val % 100000 == 0:
+            print(val)
+    return val_after_0
+
+
 def main():
     step = 349
-    n = 2017
-    value, buffer = calc_spinlock(n, step)
-    print(value)
+    n = 50000000
+    value0 = spinlock_track_0(n, step)
+
+    print(value0)
 
 
 if __name__ == '__main__':
--- 2017/day17/day17_tests.py
+++ 2017/day17/day17_tests.py
@@ -20,3 +20,32 @@ def test_part1():
     step = 349
     value, _ = day17.calc_spinlock(n, step)
     assert value == 640
+
+
+def test_basic_track_0_1():
+    n = 9
+    step = 3
+    _, buffer = day17.calc_spinlock(n, step)
+    basic_after_0 = buffer[buffer.index(0)+1]
+    assert basic_after_0 == 9
+
+
+def test_track_0_1():
+    n = 9
+    step = 3
+    assert day17.spinlock_track_0(n, step) == 9
+
+
+def test_track_0_2017():
+    n = 2017
+    step = 3
+    _, buffer = day17.calc_spinlock(n, step)
+    basic_after_0 = buffer[buffer.index(0)+1]
+    print(basic_after_0)
+    assert basic_after_0 == day17.spinlock_track_0(n, step)
+
+
+def test_part():
+    n = 50000000
+    step = 349
+    assert day17.spinlock_track_0(n, step) == 47949463
