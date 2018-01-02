f0781ccdd2240556010667311b5d9bdba16d47b7
Ed Rogers <etr@orc.soton.ac.uk>
2018-01-02 21:54:14 +0000

    2017 Day 18 part 1

--- 2017/day18/day18.py
+++ 2017/day18/day18.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 
 
 class SoundCard:
@@ -6,12 +6,12 @@ class SoundCard:
     def __init__(self, instruction_list):
         self.last_played = None
         self.registers = defaultdict(lambda: 0)
-        self.instruction_dict = {'snd': self.sound,
+        self.instruction_dict = {'snd': self.snd,
                                  'set': self.set,
                                  'add': self.add,
                                  'mul': self.multiply,
                                  'mod': self.modulo,
-                                 'rcv': self.recover,
+                                 'rcv': self.rcv,
                                  'jgz': self.jump,
                                  }
         self.instructions = [self.parse_instruction(i) for i in instruction_list]
@@ -20,10 +20,13 @@ class SoundCard:
         self.jumped = False
 
     def execute(self):
+        # if self.pointer >= len(self.instructions):
+        #     raise IndexError
+        #     return None
         self.finished = False
-        self.pointer = 0
         retval = None
         while retval is None:
+
             instruction = self.instructions[self.pointer]
             # print(instruction)
             retval = instruction['func'](*instruction['args'])
@@ -39,7 +42,7 @@ class SoundCard:
             val = self.registers[value]
         return val
 
-    def sound(self, value):
+    def snd(self, value):
         val = self.get_value(value)
         self.last_played = val
         print('Playing: %d' % val)
@@ -56,13 +59,13 @@ class SoundCard:
     def modulo(self, register, value):
         self.registers[register] %= self.get_value(value)
 
-    def recover(self, register):
+    def rcv(self, register):
         if self.registers[register] != 0:
             print('Recovered value: %d' % self.last_played)
             return self.last_played
 
     def jump(self, register, value):
-        if self.registers[register] != 0:
+        if self.get_value(register) > 0:
             self.pointer += self.get_value(value)
             self.jumped = True
 
@@ -74,13 +77,60 @@ class SoundCard:
         return instruction
 
 
+class Program(SoundCard):
+    def __init__(self, instruction_list, prog_id):
+        super().__init__(instruction_list)
+        self.prog_id = prog_id
+        self.queue = deque([])
+        self.send_counter = 0
+        self.registers['p'] = prog_id
+        self.cluster = None
+
+    def snd(self, value):  # send
+        val = self.get_value(value)
+        id_to_send_to = 1 if self.prog_id == 0 else 0
+        prog_to_send_to = self.cluster.get_prog(id_to_send_to)
+        prog_to_send_to.queue.append(val)
+        self.send_counter += 1
+
+    def rcv(self, register):  # receive
+        if self.queue:
+            self.registers[register] = self.queue.popleft()
+        else:
+            self.jumped = True  # prevent pointer increment, as instruction halted before execution
+            return True  # returning anything other than None breaks out of execute loop
+
+
+class Cluster:
+    def __init__(self, prog_list):
+        self.prog_list = prog_list
+        for p in prog_list:
+            p.cluster = self
+
+    def get_prog(self, prog_id):
+        for p in self.prog_list:
+            if p.prog_id == prog_id:
+                return p
+        return None
+
+    def execute(self):
+        deadlocked = False
+        while not deadlocked:
+            for p in self.prog_list:
+                p.execute()
+            deadlocked = all([not p.queue for p in self.prog_list])
+
+
 def main():
     with open('input.txt', 'r') as file:
         instructions = file.readlines()
     instructions = [line.strip() for line in instructions]
     print(instructions)
-    snd = SoundCard(instructions)
-    print(snd.execute())
+    prog0 = Program(instructions, 0)
+    prog1 = Program(instructions, 1)
+    cluster = Cluster([prog0, prog1])
+    cluster.execute()
+    print(prog1.send_counter)
 
 
 if __name__ == '__main__':
--- 2017/day18/day18_tests.py
+++ 2017/day18/day18_tests.py
@@ -15,3 +15,25 @@ def test_part1():
     instructions = [line.strip() for line in instructions]
     snd = day18.SoundCard(instructions)
     assert snd.execute() == 4601
+
+
+def test_part2a():
+    with open('test_input2.txt', 'r') as file:
+        instructions = file.readlines()
+    instructions = [line.strip() for line in instructions]
+    prog0 = day18.Program(instructions, 0)
+    prog1 = day18.Program(instructions, 1)
+    cluster = day18.Cluster([prog0, prog1])
+    cluster.execute()
+    assert prog1.send_counter == 3
+
+
+def test_part2():
+    with open('input.txt', 'r') as file:
+        instructions = file.readlines()
+    instructions = [line.strip() for line in instructions]
+    prog0 = day18.Program(instructions, 0)
+    prog1 = day18.Program(instructions, 1)
+    cluster = day18.Cluster([prog0, prog1])
+    cluster.execute()
+    assert prog1.send_counter == 6858
