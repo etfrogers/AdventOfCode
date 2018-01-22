c29a3c365d17c34307f39b7b0a3a4c2a69573ec0
Ed Rogers <etr@orc.soton.ac.uk>
2018-01-22 10:07:44 +0000

    Remove relative imports and replace with package level ones

--- AOC2017/day23/day23.py
+++ AOC2017/day23/day23.py
@@ -1,5 +1,3 @@
-import os
-import sys
 from collections import defaultdict
 from AOC2017.day18 import day18
 
@@ -31,14 +29,96 @@ class CoProc(day18.SoundCard):
             self.jumped = True
 
 
+def rawcode():
+    mul_counter = 0
+    a = b = c = d = e = f = g = h = 0
+    b = 84
+    c = b
+    # a = 1 for part 2
+    if a != 0:
+        b *= 100; mul_counter += 1
+        b -= -100000
+        c = b
+        c -= -17000
+    while True:
+        f = 1
+        d = 2
+        e = 2
+        done1 = False
+        done2 = False
+        while not done2:
+            while not done1:
+                g = d
+                g *= e; mul_counter += 1
+                g -= b
+                if g == 0:
+                    f = 0
+                e -= -1
+                g = e
+                g -= b
+                done1 = g == 0
+            d -= -1
+            g = d
+            g -= b
+            done2 = g == 0
+
+        if f == 0:
+            h -= -1
+        g = b
+        g -= c
+
+        if g == 0:
+            break
+        b -= -17
+    print(a, b, c, d, e, f, g, h)
+    return mul_counter, ()
+
+
+def rawcode2(b_in=None, end_in=None):
+
+    # noinspection PyUnusedLocal
+    a = b = end_search = d = e = found_multiple = g = counter = 0
+    a = 0  # for part 2
+    # above is Ed's code
+    b = 84 if b_in is None else b_in
+    end_search = b if end_in is None else end_in
+    if not a == 0:  # jnz a 2, jnz 1 5
+        b = (b * 100) + 100000
+        end_search = b + 17000 if end_in is None else end_in
+    while True:
+        found_multiple = 1
+        for d in range(2, b):
+            # print(a, b, end_search, d, e, found_multiple, g, counter)
+            for e in range(2, b):
+                if d * e == b:
+                    found_multiple = 0
+            # print(a, b, end_search, d, e, found_multiple, g, counter)
+            # print()
+        e += 1  # to make registers match
+        d += 1  # to make registers match
+        if found_multiple == 0:
+            counter += 1
+        if b >= end_search:
+            break
+        b += 17
+
+    registers = (a, b, end_search, d, e, found_multiple, g, counter)
+    print(registers)
+    return None, registers
+
+
 def main():
     with open('input.txt', 'r') as file:
         instructions = file.readlines()
     instructions = [line.strip() for line in instructions]
     print(instructions)
     coproc = CoProc(instructions)
-    coproc.execute()
+    coproc.registers['a'] = 0
+    coproc.execute(show_status=False)
+    print(coproc.register_string)
     print(coproc.mul_count)
+    mul = rawcode2(84, 84+17*3)
+    print(mul)
 
 
 if __name__ == '__main__':
--- AOC2017/day23/day23_tests.py
+++ AOC2017/day23/day23_tests.py
@@ -1,4 +1,4 @@
-import day23
+from AOC2017.day23 import day23
 
 
 def test_part1():
@@ -8,3 +8,33 @@ def test_part1():
     coproc = day23.CoProc(instructions)
     coproc.execute()
     assert coproc.mul_count == 6724
+
+
+def test_rawcode_part1():
+    mul, _ = day23.rawcode2()
+    assert mul is None or mul == 6724
+
+
+def test_multi_b_values():
+
+    for b in range(80, 100, 1):
+        for end in range(b, b+(17*3), 17):
+            yield rawcode_equals_complex_code, b, end
+
+
+def rawcode_equals_complex_code(b, c=None):
+    _, registers_raw = day23.rawcode2(b_in=b, end_in=c)
+    with open('input.txt', 'r') as file:
+        instructions = file.readlines()
+    instructions = [line.strip() for line in instructions]
+    instructions[0] = 'set b ' + str(b)
+    if c is not None:
+        instructions[1] = 'set c ' + str(c)
+    coproc = day23.CoProc(instructions)
+    coproc.execute()
+    reg_list = sorted(coproc.registers.items())
+    registers_complex = (v for k, v in reg_list)
+    registers_complex = tuple(registers_complex)
+    print(registers_raw)
+    print(registers_complex)
+    assert all([c == r for c, r in zip(registers_complex, registers_raw)])
