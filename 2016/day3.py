a106b363cffde3d902dccfc00894640c1d93a83c
Ed Rogers <etr@orc.soton.ac.uk>
2018-01-23 17:31:42 +0000

    2016 Cleanup day 3

--- 2016/day3.py
+++ 2016/day3.py
@@ -8,29 +8,35 @@ Created on Fri Nov 24 19:16:10 2017
 
 import numpy as np
 
-def istriangle(line):
+
+def is_triangle(line):
     assert(len(line) == 3)
-    #print(line)
-    maxind = np.argmax(line)
-    hyp = line[maxind]
-    sideinds =np.array([i for i in range(3) if not i == maxind])
-    sides = line[sideinds]
-    #print(hyp)
-    #print(sides)
-    isValid = (hyp < sum(sides))
-    #print(isValid)
-    return isValid
+    # print(line)
+    max_ind = np.argmax(line)
+    hyp = line[max_ind]
+    side_inds = np.array([i for i in range(3) if not i == max_ind])
+    sides = line[side_inds]
+    # print(hyp)
+    # print(sides)
+    is_valid = (hyp < sum(sides))
+    # print(is_valid)
+    return is_valid
 
-data = np.loadtxt('day3input.txt')
-print(data)
-data = np.reshape(data.T, [-1, 3])
-#print(data)
-#data = np.reshape(data, [3,-1])
-print(data)
-count = 0;
-for line in data:
-    if istriangle(line):
-        count+=1
-    
-print(count)
 
+def main():
+    data = np.loadtxt('day3input.txt')
+    print(data)
+    data = np.reshape(data.T, [-1, 3])
+    # print(data)
+    # data = np.reshape(data, [3,-1])
+    print(data)
+    count = 0
+    for line in data:
+        if is_triangle(line):
+            count += 1
+
+    print(count)
+
+
+if __name__ == '__main__':
+    main()
