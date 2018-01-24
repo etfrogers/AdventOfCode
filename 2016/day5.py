2c5c4ef7a1d15b39d8169342e13201e1f43da4af
Ed Rogers <etr@orc.soton.ac.uk>
2018-01-24 21:04:10 +0000

    2016 Day 5 part 2

--- 2016/day5.py
+++ 2016/day5.py
@@ -1,28 +1,46 @@
 import hashlib
 
 
-def get_next_char(door_id):
+def get_next_char(door_id, part2=False):
     i = 0
     while True:
         hasher = hashlib.md5()
         hasher.update((door_id + str(i)).encode('ascii'))
         hash = hasher.hexdigest()
         if hash[:5] == '00000':
-            yield hash[5]
+            if part2:
+                try:
+                    pos = int(hash[5])
+                except ValueError:
+                    pass
+                char = hash[6]
+                if pos < 8:
+                    yield pos, char
+            else:
+                yield hash[5]
         i += 1
 
 
-def get_password(door_id):
-    pw = []
-    pw_chars = get_next_char(door_id)
-    while len(pw) < 8:
-        pw.append(pw_chars.__next__())
-        print(pw)
+def get_password(door_id, part2=False):
+    pw_chars = get_next_char(door_id, part2)
+    if part2:
+        pw = ['*' for _ in range(8)]
+        while '*' in pw:
+            pos, char = pw_chars.__next__()
+            if pw[pos] == '*':
+                pw[pos] = char
+            print(pw)
+    else:
+        pw = []
+        while len(pw) < 8:
+            pw.append(pw_chars.__next__())
+            print(pw)
     return ''.join(list(pw))
 
 
 def main():
-    pw = get_password('uqwqemis')
+    pw = get_password('uqwqemis', part2=True)
+    # pw = get_password('abc', part2=True)
     print(pw)
 
 
--- 2016/day5_tests.py
+++ 2016/day5_tests.py
@@ -11,3 +11,15 @@ def test_part1():
     door_id = 'uqwqemis'
     pw = day5.get_password(door_id)
     assert pw == '1a3099aa'
+
+
+def test_whole_password2():
+    door_id = 'abc'
+    pw = day5.get_password(door_id, part2=True)
+    assert pw == '05ace8e3'
+
+
+def test_part2():
+    door_id = 'uqwqemis'
+    pw = day5.get_password(door_id, part2=True)
+    assert pw == '694190cd'
