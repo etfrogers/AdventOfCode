2015f112d9875a40477dad74f2ea10070a083b7c
Ed Rogers <etr@orc.soton.ac.uk>
2018-01-25 08:53:14 +0000

    2016 Day 6 part 2

--- 2016/day6.py
+++ 2016/day6.py
@@ -1,17 +1,20 @@
 from collections import Counter
 
 
-def unscramble(scrambled):
+def unscramble(scrambled, modified=False):
     scr_list = [list(line.strip()) for line in scrambled]
     scr_stream = zip(*scr_list)
-    plaintext = [Counter(col).most_common(1)[0][0] for col in scr_stream]
+    if modified:
+        plaintext = [Counter(col).most_common()[-1][0] for col in scr_stream]
+    else:
+        plaintext = [Counter(col).most_common(1)[0][0] for col in scr_stream]
     return ''.join(plaintext)
 
 
 def main():
     with open('day6_input.txt') as file:
         scrambled = file.readlines()
-    plaintext = unscramble(scrambled)
+    plaintext = unscramble(scrambled, True)
     print(plaintext)
 
 
--- 2016/day6_tests.py
+++ 2016/day6_tests.py
@@ -13,3 +13,17 @@ def test_part1():
         scrambled = file.readlines()
     plaintext = day6.unscramble(scrambled)
     assert plaintext == 'qrqlznrl'
+
+
+def test_2():
+    with open('day6_test_input.txt') as file:
+        scrambled = file.readlines()
+    plaintext = day6.unscramble(scrambled, modified=True)
+    assert plaintext == 'advent'
+
+
+def test_part2():
+    with open('day6_input.txt') as file:
+        scrambled = file.readlines()
+    plaintext = day6.unscramble(scrambled, modified=True)
+    assert plaintext == 'kgzdfaon'
