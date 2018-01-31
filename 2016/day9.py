a9c0b93afe85fcf4ccaa919b21c3c5c1846737bf
Ed Rogers <etr@orc.soton.ac.uk>
2018-01-31 09:35:37 +0000

    2016 Day 9 refactor length calc to be more intuitive

--- 2016/day9.py
+++ 2016/day9.py
@@ -6,10 +6,11 @@ compression_marker = re.compile(r'\((\d+)x(\d+)\)')
 def decompress(compressed, v2=False):
     dec = ''
     pointer = 0
-    dec_length = len(compressed)
+    dec_length = 0
     while True:
         marker = compression_marker.search(compressed, pointer)
         if not marker:
+            dec_length += len(compressed) - pointer
             if not v2:
                 dec += compressed[pointer:]
             break
@@ -20,8 +21,8 @@ def decompress(compressed, v2=False):
             dec += compressed[pointer:marker_start]
             to_rep = compressed[marker_start+len(marker[0]):marker_start+len(marker[0])+rep_length]
             dec += to_rep * reps
-        dec_length -= len(marker[0])
-        dec_length += rep_length*(reps-1)  # we only add extension here
+        dec_length += marker_start-pointer
+        dec_length += rep_length*reps
         pointer = marker_start+len(marker[0])+rep_length
     return dec, dec_length
 
--- 2016/day9_tests.py
+++ 2016/day9_tests.py
@@ -55,3 +55,29 @@ def test_part1():
         compressed = file.read().strip()
     _, length = day9.decompress(compressed)
     assert length == 102239
+
+
+def test_v2_1():
+    compressed = '(3x3)XYZ'  # still becomes XYZXYZXYZ, as the decompressed section contains no markers.
+    _, length = day9.decompress(compressed, v2=True)
+    assert length == len('XYZXYZXYZ')
+
+
+def test_v2_2():
+    compressed = 'X(8x2)(3x3)ABCY'  # becomes XABCABCABCABCABCABCY, because the decompressed data from the (8x2)
+    # marker is then further decompressed, thus triggering the (3x3) marker twice for a total of six ABC sequences.
+    _, length = day9.decompress(compressed, v2=True)
+    assert length == len('XABCABCABCABCABCABCY')
+
+
+def test_v2_3():
+    compressed = '(27x12)(20x12)(13x14)(7x10)(1x12)A'  # decompresses into a string of A repeated 241920 times.
+    _, length = day9.decompress(compressed, v2=True)
+    assert length == 241920
+
+
+def test_v2_4():
+    compressed = '(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN'  # becomes 445 characters long.
+    _, length = day9.decompress(compressed, v2=True)
+    assert length == 445
+
