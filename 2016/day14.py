1188ad69fc186c945961d0327d6a230f276bf89f
Ed Rogers <etr@orc.soton.ac.uk>
2018-02-06 20:52:31 +0000

    2016 Day 14 part 2

--- 2016/day14.py
+++ 2016/day14.py
@@ -15,42 +15,49 @@ def has_quint(candidate, val):
     return val*5 in candidate
 
 
-def next_1000_have_quint(salt, val, start):
+def next_1000_have_quint(salt, val, start, stretch):
     for i in range(start+1, start+1001):
-        candidate = get_hash(salt, i)
+        candidate = get_hash(salt, i, stretch)
         if has_quint(candidate, val):
             return True
     return False
 
 
-def generate_keys(salt, n=1):
+def generate_keys(salt, n=1, stretch=False):
     keys = []
     i = 0
     while len(keys) < n:
-        candidate = get_hash(salt, i)
+        candidate = get_hash(salt, i, stretch)
         triple, val = contains_triple(candidate)
         if triple:
-            if next_1000_have_quint(salt, val, i):
+            if next_1000_have_quint(salt, val, i, stretch):
                 keys.append((i, candidate))
         i += 1
     return keys
 
 
-def get_hash(salt, i):
+def get_hash(salt, i, stretch=False):
+    key = salt+str(stretch)
     try:
-        hash_dict = get_hash.hashes[salt]
+        hash_dict = get_hash.hashes[key]
     except KeyError:
-        hash_dict = get_hash.hashes[salt] = {}
+        hash_dict = get_hash.hashes[key] = {}
 
     try:
         return hash_dict[i]
     except KeyError:
-        hasher = hashlib.md5()
-        data = (salt + str(i)).encode('ascii')
-        hasher.update(data)
-        hash = hasher.hexdigest()
+        hash = one_hash(i, salt, stretch)
         hash_dict[i] = hash
         return hash
+
+
+def one_hash(i, salt, stretch):
+    data = (salt + str(i)).encode('ascii')
+    hash = hashlib.md5(data).hexdigest()
+    if stretch:
+        for _ in range(2016):  # already done one round of hashing and we need a total of 2017
+            hash = hashlib.md5(hash.encode('ascii')).hexdigest()
+    return hash
 
 
 get_hash.hashes = dict()
@@ -62,7 +69,7 @@ def main():
     # candidate = get_hash(salt, 18)
     # triple, val = contains_triple(candidate)
     # print(triple, val)
-    keys = generate_keys(salt, 64)
+    keys = generate_keys(salt, 64, stretch=True)
     print(keys[-1])
 
 
--- 2016/day14_tests.py
+++ 2016/day14_tests.py
@@ -31,3 +31,36 @@ def test_part1():
     salt = 'zpqevtbw'
     keys = day14.generate_keys(salt, 64)
     assert keys[-1][0] == 16106
+
+
+def test_stretching():
+    salt = 'abc'
+    candidate = day14.get_hash(salt, 0, stretch=True)
+    assert candidate == 'a107ff634856bb300138cac6568c0f24'
+
+
+def test_stretching_triple():
+    salt = 'abc'
+    candidate = day14.get_hash(salt, 5, stretch=True)
+    triple, val = day14.contains_triple(candidate)
+    assert triple
+    assert val == '2'
+
+
+def test_stretch_1():
+    salt = 'abc'
+    keys = day14.generate_keys(salt, 1, stretch=True)
+    assert keys[0][0] == 10
+
+
+def test_stretching_64():
+    salt = 'abc'
+    keys = day14.generate_keys(salt, 64, stretch=True)
+    assert keys[-1][0] == 22551
+
+
+def test_part2():
+    salt = 'zpqevtbw'
+    keys = day14.generate_keys(salt, 64, stretch=True)
+    assert keys[-1][0] == 22423
+
