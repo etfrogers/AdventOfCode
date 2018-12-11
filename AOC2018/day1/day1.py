6b99bfde22863b8b36b3e44b2a97b15cb440f3bf
Ed Rogers <etfrogers@hotmail.com>
2018-12-11 10:59:58 +0000

    2018 Day 1 part 2

--- AOC2018/day1/day1.py
+++ AOC2018/day1/day1.py
@@ -1,12 +1,33 @@
 from typing import Union, List
 
 
-def parse_frequency_changes(starting_value: int, changes: Union[str, List[str]]):
+def normalise_changes(changes: Union[str, List[str]]):
     if isinstance(changes, str):
         changes = changes.split(', ')
     changes = [int(v) for v in changes]
+    return changes
+
+
+def parse_frequency_changes(starting_value: int, changes: Union[str, List[str]]):
+    changes = normalise_changes(changes)
     value = starting_value
     for change in changes:
+        value += change
+    return value
+
+
+def find_repeated_freq(starting_value: int, changes: Union[str, List[str]]):
+    changes = normalise_changes(changes)
+    value = starting_value
+    prev_vals = []
+    iterator = iter(changes)
+    while value not in prev_vals:
+        try:
+            change = next(iterator)
+        except StopIteration:
+            iterator = iter(changes)
+            change = next(iterator)
+        prev_vals.append(value)
         value += change
     return value
 
@@ -15,4 +36,7 @@ if __name__ == '__main__':
     with open('input.txt') as f:
         input = f.readlines()
     f = parse_frequency_changes(0, input)
+    print(f)
+
+    f = find_repeated_freq(0, input)
     print(f)
--- AOC2018/day1/test_day1.py
+++ AOC2018/day1/test_day1.py
@@ -23,3 +23,34 @@ def test4():
     input = '-1, -2, -3'
     v = day1.parse_frequency_changes(0, input)
     assert v == -6
+
+
+def test_part2_1():
+    input = '+1, -2, +3, +1'
+    v = day1.find_repeated_freq(0, input)
+    assert v == 2
+
+
+def test_part2_2():
+    input = '+1, -1'
+    v = day1.find_repeated_freq(0, input)
+    assert v == 0
+
+
+def test_part2_3():
+    input = '+3, +3, +4, -2, -4'
+    v = day1.find_repeated_freq(0, input)
+    assert v == 10
+
+
+def test_part2_4():
+    input = '-6, +3, +8, +5, -6'
+    v = day1.find_repeated_freq(0, input)
+    assert v == 5
+
+
+def test_part2_5():
+    input = '+7, +7, -2, -7, -4'
+    v = day1.find_repeated_freq(0, input)
+    assert v == 14
+
