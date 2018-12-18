79f31a83d524a3c7212204b3b0fa9cf53bd85402
Ed Rogers <etfrogers@hotmail.com>
2018-12-18 16:52:46 +0000

    2018 Day 7 part 2

--- AOC2018/day7/day7.py
+++ AOC2018/day7/day7.py
@@ -6,9 +6,14 @@ class Step:
         self.label = label
         self.pre_requisites = set()
 
+    @property
+    def time(self):
+        return StepFactory.BASE_TIME + 1 + ord(self.label) - ord('A')
+
 
 class StepFactory:
     PATTERN = re.compile('Step ([A-Z]) must be finished before step ([A-Z]) can begin.')
+    BASE_TIME = 60
     steps = {}
 
     @staticmethod
@@ -33,35 +38,104 @@ class StepFactory:
         return matches.groups()
 
 
+class Worker:
+    def __init__(self):
+        self.task: Step = None
+        self.time_worked = 0
+
+    @property
+    def has_finished(self):
+        return self.time_worked >= self.task.time if self.task else False
+
+    def reset(self):
+        self.task = None
+        self.time_worked = 0
+
+    @property
+    def is_assigned(self):
+        return self.task is not None
+
+    def tick(self):
+        if self.is_assigned:
+            self.time_worked += 1
+
+    def assign_task(self, task: Step):
+        self.reset()
+        self.task = task
+
+
 class Plan:
-    def __init__(self, list_of_instructions):
+    def __init__(self, list_of_instructions, n_workers=None):
         self.completed = set()
+        self.time = 0
         self.steps = set()
+        self.workers = None
+        if n_workers:
+            self.workers = [Worker() for _ in range(n_workers)]
 
         for instruction in list_of_instructions:
             self.steps.update(StepFactory.create_step(instruction))
 
     def ready_steps(self):
-        ready = {step.label for step in self.steps if not step.pre_requisites - self.completed}
-        ready = ready - self.completed
+        ready = {step for step in self.steps if not step.pre_requisites - self.completed
+                                                and step.label not in self.completed
+                                                and step not in self.in_progress}
+
         return ready
 
     def reset(self):
         self.completed = set()
+        self.time = 0
 
     def execute(self):
         execution = []
-        while len(execution) < len(self.steps):
-            feasible = list(self.ready_steps())
-            next_step = sorted(feasible)[0]
-            self.completed.add(next_step)
-            execution.append(next_step)
+        while not self.finished:
+            next_step = self.next_step
+            self.completed.add(next_step.label)
+            execution.append(next_step.label)
         return ''.join(execution)
+
+    @property
+    def next_step(self):
+        feasible = list(self.ready_steps())
+        return sorted(feasible, key=lambda step: step.label)[0] if feasible else None
+
+    @property
+    def finished(self):
+        return len(self.completed) == len(self.steps)
+
+    @property
+    def in_progress(self):
+        return {worker.task for worker in self.workers} if self.workers else set()
+
+    def tick(self):
+        self.time += 1
+        for worker in self.workers:
+            worker.tick()
+            if worker.has_finished:
+                self.completed.add(worker.task.label)
+                worker.reset()
+        self.assign_workers()
+
+    def assign_workers(self):
+        for worker in self.workers:
+            if not worker.is_assigned:
+                worker.assign_task(self.next_step)
+
+    def execute_timed(self):
+        self.reset()
+        self.assign_workers()
+        while not self.finished:
+            self.tick()
+        return self.time
 
 
 if __name__ == '__main__':
     with open('input.txt') as f:
         input_ = f.readlines()
     input_ = [line.strip() for line in input_]
-    plan = Plan(input_)
+    plan = Plan(input_, 5)
     print(plan.execute())
+
+    plan.reset()
+    print(plan.execute_timed())
--- AOC2018/day7/test_day7.py
+++ AOC2018/day7/test_day7.py
@@ -13,9 +13,20 @@ input = input.split('\n')
 
 def test1():
     plan = day7.Plan(input)
-    assert plan.ready_steps() == set('C')
+    assert {step.label for step in plan.ready_steps()} == set('C')
 
 
 def test2():
     plan = day7.Plan(input)
     assert plan.execute() == 'CABDFE'
+
+
+def test3():
+    assert day7.StepFactory.get_step('A').time == 61
+    assert day7.StepFactory.get_step('Z').time == 86
+
+
+def test4():
+    day7.StepFactory.BASE_TIME = 0
+    plan = day7.Plan(input, n_workers=2)
+    assert plan.execute_timed() == 15
