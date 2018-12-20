942a7cf89112150a8651180a39e84860fab2f589
Ed Rogers <etfrogers@hotmail.com>
2018-12-20 15:12:03 +0000

    2018 Day 11 part 2

--- AOC2018/day11/day11.py
+++ AOC2018/day11/day11.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.signal import convolve2d
+from scipy.signal import convolve
 
 RACK_SIZE = (300, 300)
 REGION = np.ones((3, 3))
@@ -19,9 +19,32 @@ class Rack:
     def __getitem__(self, item):
         return self.power_levels[item[1] - 1, item[0] - 1]
 
-    def best_region(self):
-        region_values = convolve2d(self.power_levels, REGION, mode='same')
-        return tuple(reversed(np.unravel_index(np.argmax(region_values), region_values.shape)))
+    def best_region(self, size=None):
+        if size is None:
+            region = REGION
+            size = 3
+        else:
+            region = np.ones((size, size))
+        return find_max_region(self.power_levels, region)
+
+    def best_region_scan_size(self):
+        sizes = range(1, RACK_SIZE[0] + 1)
+        best_coords, best_vals = zip(*[self.best_region(size) for size in sizes])
+        max_index = np.argmax(best_vals)
+        return best_coords[max_index] + (sizes[max_index], )
+
+
+def find_max_region(array, region):
+    size = region.shape[0]
+    assert all([s == size for s in region.shape])
+    region_values = convolve(array, region, mode='same')
+    # round because convolve can use fft which gives floats. In this case we know all vals are integer
+    region_values = np.round(region_values).astype(int)
+    best_val = np.max(region_values)
+    best_coords = tuple(reversed(np.unravel_index(np.argmax(region_values), region_values.shape)))
+    # convert for top left corner
+    best_coords = tuple(c + 1 - int(np.floor(size / 2)) for c in best_coords)
+    return best_coords, best_val
 
 
 def hundreds_digit(val):
@@ -32,3 +55,5 @@ def hundreds_digit(val):
 if __name__ == '__main__':
     rack = Rack(3999)
     print(rack.best_region())
+
+    print(rack.best_region_scan_size())
--- AOC2018/day11/test_day11.py
+++ AOC2018/day11/test_day11.py
@@ -1,5 +1,5 @@
 from AOC2018.day11 import day11
-
+import numpy as np
 
 def test1():
     rack = day11.Rack(8)
@@ -23,9 +23,29 @@ def test4():
 
 def test5():
     rack = day11.Rack(18)
-    assert rack.best_region() == (33, 45)
+    assert rack.best_region() == ((33, 45), 29)
 
 
 def test6():
     rack = day11.Rack(42)
-    assert rack.best_region() == (21, 61)
+    assert rack.best_region() == ((21, 61), 30)
+
+
+def test_part_2_1():
+    rack = day11.Rack(18)
+    assert rack.best_region_scan_size() == (90, 269, 16)
+
+
+def test_part_2_2():
+    rack = day11.Rack(42)
+    assert rack.best_region_scan_size() == (232, 251, 12)
+
+
+def test_part1():
+    rack = day11.Rack(3999)
+    assert rack.best_region()[0] == (21, 77)
+
+
+def test_part2():
+    rack = day11.Rack(3999)
+    assert rack.best_region_scan_size() == (224, 222, 27)
