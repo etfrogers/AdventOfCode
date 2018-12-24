492a3d7f84182ac60f26777c1795cc2de42cb9b0
Ed Rogers <etfrogers@hotmail.com>
2018-12-24 16:07:52 +0000

    2018 Day 16 part 1

---
+++ AOC2018/day16/day16.py
@@ -0,0 +1,94 @@
+import math
+import re
+from AOC2018.day16 import reddit_day16
+
+PATTERNS = [re.compile(r'Before: \[(\d+), (\d+), (\d+), (\d+)\]'),
+            re.compile(r'(\d+) (\d+) (\d+) (\d+)'),
+            re.compile(r'After:  \[(\d+), (\d+), (\d+), (\d+)\]')]
+
+
+class Sample:
+    def __init__(self, input_):
+        matches = [pat.match(line) for pat, line in zip(PATTERNS, input_)]
+        self.before_state = tuple((int(g) for g in matches[0].groups()))
+        self.opcode_data = tuple((int(g) for g in matches[1].groups()))
+        self.after_state = tuple((int(g) for g in matches[2].groups()))
+
+
+class Opcode:
+    def __init__(self,label, func, derefA, derefB, useB=True):
+        self.label = label
+        self.func = func
+        self.derefA = derefA
+        self.derefB = derefB
+        self.useB = useB
+
+    def __call__(self, input_state, command, *args, **kwargs):
+        output_state = list(input_state)
+        if self.derefA:
+            a = input_state[command[1]]
+        else:
+            a = command[1]
+        if self.derefB:
+            b = input_state[command[2]]
+        else:
+            b = command[2]
+        if self.func is None:
+            assert not self.useB
+            val = a
+        elif self.useB:
+            val = self.func(a, b)
+        else:
+            val = self.func(a)
+        output_state[command[3]] = int(val)
+        return tuple(output_state)
+
+
+class Device:
+    opcodes = {'addr': Opcode('addr', int.__add__, derefA=True, derefB=True),
+               'addi': Opcode('addi', int.__add__, derefA=True, derefB=False),
+               'mulr': Opcode('mulr', int.__mul__, derefA=True, derefB=True),
+               'muli': Opcode('muli', int.__mul__, derefA=True, derefB=False),
+               'banr': Opcode('banr', int.__and__, derefA=True, derefB=True),
+               'bani': Opcode('bani', int.__and__, derefA=True, derefB=False),
+               'borr': Opcode('borr', int.__or__, derefA=True, derefB=True),
+               'bori': Opcode('bori', int.__or__, derefA=True, derefB=False),
+               'setr': Opcode('setr', None, derefA=True, derefB=None, useB=False),
+               'seti': Opcode('seti', None, derefA=False, derefB=None, useB=False),
+               'gtir': Opcode('gtir', int.__gt__, derefA=False, derefB=True),
+               'gtri': Opcode('gtri', int.__gt__, derefA=True, derefB=False),
+               'gtrr': Opcode('gtrr', int.__gt__, derefA=True, derefB=True),
+               'eqir': Opcode('eqir', int.__eq__, derefA=False, derefB=True),
+               'eqri': Opcode('eqri', int.__eq__, derefA=True, derefB=False),
+               'eqrr': Opcode('eqrr', int.__eq__, derefA=True, derefB=True),
+               }
+    assert len(opcodes) == 16
+
+    def n_opcodes(self, sample):
+        return sum([1 for opcode in self.opcodes.values() if self.opcode_works(opcode, sample)])
+
+    @staticmethod
+    def opcode_works(opcode, sample):
+        # return all(t == v for t, v in zip(sample.after_state, opcode(sample.before_state, sample.opcode_data)))
+        return sample.after_state == opcode(sample.before_state, sample.opcode_data)
+
+
+def input_to_samples(input_):
+    data = []
+    for i in range(math.ceil(len(input_) / 4)):
+        data.append(input_[4 * i:4 * i + 3])
+    return [Sample(d) for d in data]
+
+
+if __name__ == '__main__':
+    device = Device()
+    with open('input.txt') as f:
+        input_ = f.read()
+    input_ = input_.split('\n\n\n\n')
+
+    input_part1, input_part2 = ([line.strip() for line in part.split('\n')] for part in input_)
+    samples = input_to_samples(input_part1)
+
+    n = sum([1 if device.n_opcodes(sample) >=3 else 0 for sample in samples ])
+
+    print(n)
---
+++ AOC2018/day16/test_day16.py
@@ -0,0 +1,83 @@
+import itertools
+
+from AOC2018.day16 import day16, reddit_day16
+
+
+test_input = '''Before: [3, 2, 1, 1]
+9 2 1 2
+After:  [3, 2, 2, 1]'''
+
+sample = day16.Sample(test_input.split('\n'))
+device = day16.Device()
+
+
+def test1():
+    assert device.n_opcodes(sample) == 3
+
+
+def test_addr():
+    input_state = (0, 1, 2, 3)
+    command = (0, 2, 3, 0)
+    val = device.opcodes['addr'](input_state, command)
+    test = (5, 1, 2, 3)
+    assert all([v == t for v, t in zip(val, test)])
+
+
+def test_setr():
+    input_state = (0, 1, 2, 3)
+    command = (0, 2, 3, 0)
+    val = device.opcodes['setr'](input_state, command)
+    test = (2, 1, 2, 3)
+    assert all([v == t for v, t in zip(val, test)])
+
+
+def test_seti():
+    input_state = (0, 1, 2, 3)
+    command = (0, 6, 3, 0)
+    val = device.opcodes['seti'](input_state, command)
+    test = (6, 1, 2, 3)
+    assert all([v == t for v, t in zip(val, test)])
+
+
+def test_eqir1():
+    input_state = (0, 1, 2, 3)
+    command = (0, 3, 3, 0)
+    val = device.opcodes['eqir'](input_state, command)
+    test = (1, 1, 2, 3)
+    assert all([v == t for v, t in zip(val, test)])
+
+
+def test_eqir2():
+    input_state = (0, 1, 2, 3)
+    command = (0, 1, 3, 0)
+    val = device.opcodes['eqir'](input_state, command)
+    test = (0, 1, 2, 3)
+    assert all([v == t for v, t in zip(val, test)])
+
+
+def test_labels():
+    assert all([key == val.label for key, val in device.opcodes.items()])
+
+
+def combi_reddit(reddit_func, input_, cmd, my_func, my_cmd):
+    assert all([r == m for r, m in zip(reddit_func(input_, *cmd), my_func(input_, my_cmd))])
+
+
+# def test_with_reddit():
+#     for op in reddit_day16.OPERATIONS:
+#         for input_ in itertools.product(range(0, 4, 2), repeat=4):
+#             for cmd in itertools.product(range(4), repeat=3):
+#                 my_cmd = (0,) + cmd
+#                 yield combi_reddit, op, list(input_), list(cmd), device.opcodes[op.__name__], list(my_cmd)
+
+
+def test_part1():
+    with open('input.txt') as f:
+        input_ = f.read()
+    input_ = input_.split('\n\n\n\n')
+
+    input_part1, input_part2 = ([line.strip() for line in part.split('\n')] for part in input_)
+    samples = day16.input_to_samples(input_part1)
+
+    n = sum([1 if device.n_opcodes(sample) >=3 else 0 for sample in samples ])
+    assert n == 493
