829054278e9a286079b7f08d97815ccc5e887931
Ed Rogers <etfrogers@hotmail.com>
2018-12-27 11:25:08 +0000

    2018 Day 6 part 1

---
+++ AOC2018/day6/day6.py
@@ -0,0 +1,77 @@
+import string
+
+import numpy as np
+
+
+class Space:
+    def __init__(self, input_, label_strs=None):
+        if type(input_) is str:
+            input_ = input_.split('\n')
+        pts = [line.split(', ') for line in input_]
+        self.points = np.array(pts, dtype=int)
+        self.labels = np.arange(1, len(self.points)+1)[np.newaxis, np.newaxis, :]
+        x_ = np.arange(np.min(self.points[:, 0]) - 1, np.max(self.points[:, 0] + 2))
+        y_ = np.arange(np.min(self.points[:, 1]) - 1, np.max(self.points[:, 1] + 2))
+        self.x, self.y = np.meshgrid(x_, y_)
+        self.map = np.zeros_like(self.x)
+        self.label_strs = label_strs
+
+    def render(self):
+        map = self.map.astype(dtype=str)
+        map[np.where(self.map == 0)] = '.'
+        for pt, label in zip(self.points, self.labels[0, 0, :]):
+            map[np.where(self.map == label)] = self.num_to_label(label, uppercase=False)
+            map[np.logical_and(self.x == pt[0], self.y == pt[1])] = self.num_to_label(label)
+        map = map.tolist()
+        map = '\n'.join([''.join(line) for line in map])
+        return map
+
+    def build_regions(self):
+        pt_x = self.points[:, 0]
+        pt_y = self.points[:, 1]
+        dists = np.abs(self.x[:, :, np.newaxis] - pt_x[np.newaxis, np.newaxis, :]) + \
+                np.abs(self.y[:, :, np.newaxis] - pt_y[np.newaxis, np.newaxis, :])
+
+        min_ind = np.argmin(dists, 2)
+        min_val = np.min(dists, 2)
+        tied = np.sum(dists == min_val[:, :, np.newaxis], 2) > 1
+        self.map = self.labels[0, 0, min_ind]
+        self.map[tied] = 0
+
+    def sizes(self):
+        finite_map = self.map.copy()
+        edge_labels = set(np.concatenate((finite_map[ 0, :].flatten(),
+                                          finite_map[-1, :].flatten(),
+                                          finite_map[:,  0].flatten(),
+                                          finite_map[:, -1].flatten())).tolist())
+
+        return {self.num_to_label(label): np.sum(self.map == label) if label not in edge_labels else -1
+                for label in self.labels.flatten().tolist()}
+
+    def num_to_label(self, num, uppercase=True):
+        if self.label_strs is not None:
+            val = self.label_strs[num-1]
+            val = val.upper() if uppercase else val.lower()
+        else:
+            val = num
+        return val
+
+    def safest_place(self):
+        sizes = self.sizes()
+        return max(sizes.keys(), key=lambda key: sizes[key])
+
+    def safest_size(self):
+        sizes = self.sizes()
+        return max(sizes.values())
+
+
+def main():
+    with open('input.txt') as f:
+        input_ = f.read()
+    space = Space(input_)
+    space.build_regions()
+    print('Part 1: ', space.safest_size())
+
+
+if __name__ == '__main__':
+    main()
---
+++ AOC2018/day6/test_day6.py
@@ -0,0 +1,78 @@
+from AOC2018.day6 import day6
+
+
+test_input = '''1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9'''
+
+
+test_output_prebuild = '''..........
+.A........
+..........
+........C.
+...D......
+.....E....
+.B........
+..........
+..........
+........F.
+..........'''
+
+
+test_ouput = '''aaaaa.cccc
+aAaaa.cccc
+aaaddecccc
+aadddeccCc
+..dDdeeccc
+bb.deEeecc
+bBb.eeee..
+bbb.eeefff
+bbb.eeffff
+bbb.ffffFf
+bbb.ffffff'''
+
+
+test_labels = 'ABCDEF'
+
+
+def test_prebuild():
+    space = day6.Space(test_input, test_labels)
+    assert space.render() == test_output_prebuild
+
+
+def test_render():
+    space = day6.Space(test_input, test_labels)
+    space.build_regions()
+    calc_output = space.render()
+    assert calc_output == test_ouput
+
+
+def test_1():
+    test_sizes = {'A': -1, 'B': -1, 'C': -1, 'D': 9, 'E': 17, 'F': -1}
+    space = day6.Space(test_input, test_labels)
+    space.build_regions()
+    sizes = space.sizes()
+    assert all([sizes[key] == test_sizes[key] for key in test_sizes.keys()])
+
+
+def test_2():
+    space = day6.Space(test_input, test_labels)
+    space.build_regions()
+    assert space.safest_place() == 'E'
+
+
+def test_3():
+    space = day6.Space(test_input, test_labels)
+    space.build_regions()
+    assert space.safest_size() == 17
+
+
+def test_part1():
+    with open('input.txt') as f:
+        input_ = f.read()
+    space = day6.Space(input_)
+    space.build_regions()
+    assert space.safest_size() == 4771
