b0973b4e54d95b2ad8cb1b31cbd3355920942466
Ed Rogers <etfrogers@hotmail.com>
2018-12-28 20:01:40 +0000

    2018 Day 19 temp commit

--- AOC2018/day19/day19.py
+++ AOC2018/day19/day19.py
@@ -1,3 +1,5 @@
+import time
+
 from AOC2018.day16 import day16
 
 
@@ -7,8 +9,10 @@ class JumpDevice(day16.Device):
         self.instruction_pointer = 0
         self.opcodes['#ip'] = None
 
-    def run(self, program, limit=None):
+    def run(self, program, limit=None, registers=None, do_print=False):
         self.reset()
+        if registers is not None:
+            self.registers = registers
         program = self.parse_program(program)
 
         ip_line = program.pop(0)
@@ -29,14 +33,22 @@ class JumpDevice(day16.Device):
             ip = self.registers[self.instruction_pointer]
             ip += 1
             counter += 1
+            if do_print:
+                print(f'{line[0]}: {line[1:]} -> {self.registers}')
+                time.sleep(0.3)
 
 
 def main():
     with open('input.txt') as f:
         program = f.read()
     device = JumpDevice()
-    device.run(program)
-    print('Part 1: ', device.registers)
+    # device.run(program, do_print=True)
+    # print('Part 1: ', device.registers)
+
+    device.reset()
+    registers = [1] + [0] * 5
+    device.run(program, registers=registers, do_print=True)
+    print('Part 2: ', device.registers)
 
 
 if __name__ == '__main__':
