22f9ced73b8a9f0837c921533a88880eb1924cf0
Ed Rogers <etfrogers@hotmail.com>
2019-01-01 10:57:09 +0000

    2018 Day 21 part 1

--- AOC2018/day21/day21.py
+++ AOC2018/day21/day21.py
@@ -3,23 +3,8 @@ from AOC2018.day19.day19 import JumpDevice
 
 
 def main():
-    with open('input.txt') as f:
-        program = f.read()
-    device = JumpDevice()
-    limit = 1000000
-    for i in range(1):
-        counter = device.run(program, registers=[i] + [0]*5, limit=limit, do_print=False, log_at_line=6)
-        if counter != limit:
-            print(i, ', ', counter)
-        else:
-            print(i)
-
-    print(device.log)
-
-    a = assembly(0)
-    b = assembly_refactor(0)
-    print(tuple(a) == tuple(b))
-    pass
+    part1 = assembly_refactor(0, 2)
+    print('Part 1: ', part1[1])
 
 
 if __name__ == '__main__':
--- AOC2018/day21/test_day21.py
+++ AOC2018/day21/test_day21.py
@@ -6,7 +6,7 @@ def test_assembly_matches_vm():
     with open('input.txt') as f:
         program = f.read()
     device = day19.JumpDevice()
-    limit = 2000000
+    limit = 500000
     for i in range(1):
         counter = device.run(program, registers=[i] + [0]*5, limit=limit, do_print=False, log_at_line=6)
         if counter != limit:
@@ -21,7 +21,25 @@ def test_assembly_matches_vm():
 
 
 def test_assembly_matches_refactor():
-    a = assembly(0, 100)
-    b = assembly_refactor(0, 100)
+    a = assembly(0, 300)
+    b = assembly_refactor(0, 300)
 
     assert tuple(b) == tuple(a)
+
+
+def test_part1():
+    a = assembly(0, 2)
+    b = assembly_refactor(0, 2)
+    with open('input.txt') as f:
+        program = f.read()
+    device = day19.JumpDevice()
+    limit = 50000
+    for i in range(1):
+        counter = device.run(program, registers=[i] + [0] * 5, limit=limit, do_print=False, log_at_line=6)
+        if counter != limit:
+            print(i, ', ', counter)
+        else:
+            print(i)
+    assert a[1] == 10504829
+    assert b[1] == 10504829
+    assert device.log[1][3] == 10504829
