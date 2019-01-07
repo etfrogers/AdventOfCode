92e6cf9383d221458750cadcb57a4cf332231736
Ed Rogers <etfrogers@hotmail.com>
2019-01-07 11:51:50 +0000

    2018 Day 15 movement example

--- AOC2018/day15/day15.py
+++ AOC2018/day15/day15.py
@@ -1,8 +1,9 @@
 from enum import Enum
-from typing import List
+from typing import List, Union
 
 import numpy as np
 from scipy.ndimage import measurements
+
 
 UNITTYPE_DICT = {2: 'E', 3: 'G'}
 UNITTYPE_DICT_INV = {item[1]: item[0] for item in UNITTYPE_DICT.items()}
@@ -12,9 +13,8 @@ class UnitType(Enum):
     ELF = 2
     GOBLIN = 3
 
-    @staticmethod
-    def to_string(value):
-        return UNITTYPE_DICT[UnitType(value).value]
+    def to_string(self):
+        return UNITTYPE_DICT[UnitType(self).value]
 
     @staticmethod
     def from_string(string):
@@ -26,19 +26,31 @@ class Square:
     def __init__(self, position):
         self.position = position
 
+    def __eq__(self, other):
+        return np.array_equal(self.position, other.position)
+
+    def __add__(self, other):
+        return Square(self.position + other.position)
+
     def distance_to(self, other):
         return np.sum(np.abs(self.position - other.position))
 
     @property
     def numpy_coord(self):
-        return np.flipud(self.position)
+        return tuple(np.flipud(self.position))
+
+    def distance_map(self, fight):
+        x = self.position[0]
+        y = self.position[1]
+        return np.abs(fight.x - x) + np.abs(fight.y - y)
 
 
-NEARBY = [Square(np.array([-1, 0])),
+NEARBY = [Square(np.array([0, -1])),  # these should be in reading order
+          Square(np.array([-1, 0])),
           Square(np.array([1, 0])),
-          Square(np.array([0, -1])),
           Square(np.array([0, 1])),
           ]
+NO_MOVEMENT = Square(np.array([0, 0]))
 
 
 class Unit(Square):
@@ -51,26 +63,43 @@ class Unit(Square):
             type_ = UnitType(type_)
         self.type = type_
 
+    def __eq__(self, other):
+        return self.type == other.type and Square.__eq__(self, other)
+
     def get_all_targets(self, fight):
         targets = []
+        found_target_unit = False
         for unit in fight.units:
             if self.is_target(unit):
+                found_target_unit = True
+                if self.distance_to(unit) <= 1:
+                    # if already next to a target unit, add own position (which should then be nearest target), as a
+                    # movement target
+                    targets.append(Square(self.position))
                 targets.extend(unit.in_range_squares(fight.map))
-        return in_reading_order(targets)
+        return in_reading_order(targets) if found_target_unit else None
 
     def is_target(self, other):
         return self.type != other.type
 
     def filter_by_reachable(self, map_: np.ndarray, target_list: List[Square]):
         reachable = self.reachable_region(map_)
-        return [target for target in target_list if reachable[tuple(target.numpy_coord)]]
+        return [target for target in target_list if reachable[target.numpy_coord]]
 
     def find_target(self, fight):
         target_list = self.get_all_targets(fight)
         target_list = self.filter_by_reachable(fight.map, target_list)
         target_list.sort(key=lambda unit: self.distance_to(unit))
+        if target_list is None:
+            # no target UNITS found
+            return None
+        if not target_list:
+            # no target SQUARES found (i.e. all enemy units are surrounded)
+            return []
         nearby_targets = [target_list[0]]
         dist = self.distance_to(target_list[0])
+        if dist == 0:
+            return Square(self.position)
         for unit in target_list:
             if self.distance_to(unit) > dist:
                 break
@@ -79,29 +108,49 @@ class Unit(Square):
         return in_reading_order(nearby_targets)[0]
 
     def in_range_squares(self, map_):
-        return in_reading_order(
-            [Square(self.position + pos.position) for pos in NEARBY
-             if map_[tuple(self.numpy_coord + pos.numpy_coord)] == 0])
+        return [Square(self.position + pos.position) for pos in NEARBY if map_[(self + pos).numpy_coord] == 0]
 
     def reachable_region(self, map_: np.ndarray):
         regions = map_.copy()
         regions[regions == 2] = 1  # remove elves
         regions[regions == 3] = 1  # remove goblins
-        regions[tuple(self.numpy_coord)] = 0  # add back self
+        regions[self.numpy_coord] = 0  # add back self
         regions = np.logical_not(regions).astype(int)
         measurements.label(regions, output=regions)
-        return regions == regions[tuple(self.numpy_coord)]
+        return regions == regions[self.numpy_coord]
+
+    def move_in(self, fight):
+        target = self.find_target(fight)
+        if target is None:
+            raise StopIteration
+        elif not target:
+            return
+        direction = self.path_to(fight, target)
+        fight.map[self.numpy_coord] = 0
+        self.position += direction.position
+        fight.map[self.numpy_coord] = self.type.value
+
+    def path_to(self, fight, target):
+        if np.array_equal(self.position, target.position):
+            return NO_MOVEMENT
+        all_dists = target.distance_map(fight)
+        dists = [all_dists[(self + pos).numpy_coord] for pos in NEARBY]
+        return NEARBY[dists.index(min(dists))]  # index returns first, but that is ok because NEARBY is in reading order
 
 
 class Fight:
 
     def __init__(self, input_map: str):
+        self.completed_rounds = 0
         array_str = input_map.replace('E', '2')
         array_str = array_str.replace('G', '3')
         array_str = array_str.replace('.', '0')
         array_str = array_str.replace('#', '1')
         array_list = [[int(v) for v in line] for line in array_str.split('\n')]
-        self.map = np.array(array_list)
+        self.map: np.ndarray = np.array(array_list)
+        x = np.arange(self.map.shape[1])
+        y = np.arange(self.map.shape[0])
+        self.x, self.y = np.meshgrid(x, y)
         self.cave = self.map.copy()
         self.cave[self.cave == 2] = 0
         self.cave[self.cave == 3] = 0
@@ -109,8 +158,26 @@ class Fight:
         goblins = [Unit(UnitType.GOBLIN, pos) for pos in np.transpose((self.map.transpose() == 3).nonzero())]
         self.units = elves + goblins
 
+    def __eq__(self, other):
+        return np.array_equal(self.map, other.map) and \
+            np.array_equal(self.cave, other.cave) and \
+            all([a == b for a, b in zip(in_reading_order(self.units), in_reading_order(other.units))])
+
     def move_list(self):
         return in_reading_order(self.units)
+
+    def do_round(self):
+        for unit in self.move_list():
+            unit.move_in(self)
+            # unit.attack_in(self)
+
+    def evolve(self, n_rounds: int=0):
+        while n_rounds < 1 or self.completed_rounds < n_rounds:
+            try:
+                self.do_round()
+            except StopIteration:
+                return
+            self.completed_rounds += 1
 
 
 def reading_order(position):
--- AOC2018/day15/test_day15.py
+++ AOC2018/day15/test_day15.py
@@ -83,3 +83,81 @@ def test_find_tgt():
     tgt = unit.find_target(fight)
     assert np.array_equal(tgt.position, np.array([3, 1]))
 
+
+# #######       #######       #######       #######       #######
+# #.E...#       #.E...#       #.E...#       #4E212#       #..E..#
+# #...?.#  -->  #...!.#  -->  #...+.#  -->  #32101#  -->  #.....#
+# #..?G?#       #..!G.#       #...G.#       #432G2#       #...G.#
+# #######       #######       #######       #######       #######
+
+def test_simple_movement():
+    input_map = '''#######
+#.E...#
+#.....#
+#...G.#
+#######'''
+    after_map = '''#######
+#..E..#
+#.....#
+#...G.#
+#######'''
+    fight = day15.Fight(input_map)
+    unit = fight.move_list()[0]
+    unit.move_in(fight)
+    fight_after = day15.Fight(after_map)
+    assert fight == fight_after
+
+
+movement_maps = '''#########
+#G..G..G#
+#.......#
+#.......#
+#G..E..G#
+#.......#
+#.......#
+#G..G..G#
+#########
+
+#########
+#.G...G.#
+#...G...#
+#...E..G#
+#.G.....#
+#.......#
+#G..G..G#
+#.......#
+#########
+
+#########
+#..G.G..#
+#...G...#
+#.G.E.G.#
+#.......#
+#G..G..G#
+#.......#
+#.......#
+#########
+
+#########
+#.......#
+#..GGG..#
+#..GEG..#
+#G..G...#
+#......G#
+#.......#
+#.......#
+#########'''
+
+
+def check_evolution(initial_map, final_map, n_rounds):
+    fight = day15.Fight(initial_map)
+    fight.evolve(n_rounds)
+    after_fight = day15.Fight(final_map)
+    assert fight == after_fight
+
+
+def test_movement():
+    maps = movement_maps.split('\n\n')
+    initial_state, *final_states = maps
+    for i, final_state in enumerate(final_states):
+        yield check_evolution, initial_state, final_state, i+1
