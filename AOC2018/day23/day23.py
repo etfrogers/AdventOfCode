63b356427701f7cf695b7cc6a5c39b652fc875fd
Ed Rogers <etfrogers@hotmail.com>
2019-01-22 13:01:31 +0000

    2018 Day 23 working on z3

--- AOC2018/day23/day23.py
+++ AOC2018/day23/day23.py
@@ -2,6 +2,8 @@ import re
 
 import numpy as np
 
+import AOC2018.day23.z3.z3 as z3
+
 
 class Nanobot:
     PATTERN = re.compile(r'pos=<(-?[\d]+),(-?[\d]+),(-?[\d]+)>, r=(-?[\d]+)')
@@ -12,7 +14,7 @@ class Nanobot:
         self.range = int(matches.groups()[3])
 
     def dist_to(self, other):
-        return np.sum(np.abs(self.pos - other.pos))
+        return distance(self.pos, other.pos)
 
     def in_range(self, bots):
         return [bot for bot in bots if self.dist_to(bot) <= self.range]
@@ -20,6 +22,29 @@ class Nanobot:
 
 def get_strongest_bot(bots):
     return max(bots, key=lambda bot: bot.range)
+
+
+def in_range_of_most(bots):
+    x, y, z = z3.Ints('x y z')
+    pt = np.array([x, y, z])
+    in_range = [zdist(pt, bot.pos) < bot.range for bot in bots]
+    z3.solve(in_range)
+    pass
+
+
+def zabs(x):
+    return z3.If(x >= 0, x, -x)
+
+
+def zdist(pos1, pos2):
+    return zabs(pos1[0] - pos2[0]) + zabs(pos1[1] - pos2[1]) + zabs(pos1[2] - pos2[2])
+
+
+def distance(pos1, pos2):
+    return np.sum(np.abs(pos1 - pos2))
+    # return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1]) + abs(pos1[2] - pos2[2])
+    # return sum([abs(p1 - p2) for p1, p2 in zip(pos1, pos2)])
+
 
 
 def main():
--- AOC2018/day23/test_day23.py
+++ AOC2018/day23/test_day23.py
@@ -25,3 +25,32 @@ def test2():
     bot = day23.get_strongest_bot(bots)
     bots_in_range = bot.in_range(bots)
     assert len(bots_in_range) == 7
+
+
+def test_part1():
+    with open('input.txt') as f:
+        specs = f.readlines()
+    bots = [day23.Nanobot(spec) for spec in specs]
+    bot = day23.get_strongest_bot(bots)
+    bots_in_range = bot.in_range(bots)
+    assert len(bots_in_range) == 433
+
+
+part2_test_input = '''pos=<10,12,12>, r=2
+pos=<12,14,12>, r=2
+pos=<16,12,12>, r=4
+pos=<14,14,14>, r=6
+pos=<50,50,50>, r=200
+pos=<10,10,10>, r=5'''
+
+
+def test_in_range_of_most():
+    bots = [day23.Nanobot(spec) for spec in part2_test_input.split('\n')]
+    point = day23.in_range_of_most(bots)
+    assert np.array_equal(point, np.array([12, 12, 12]))
+
+
+def test_in_range_of_most_and_dist():
+    bots = [day23.Nanobot(spec) for spec in part2_test_input.split('\n')]
+    point = day23.in_range_of_most(bots)
+    assert day23.distance(point, np.array([0, 0, 0])) == 36
