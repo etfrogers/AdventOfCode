ae254aeab6437a15607e1559ea2c78f29e7c77f2
Ed Rogers <etfrogers@hotmail.com>
2019-04-08 19:04:37 +0100

    Day 13 part 1: Basic evolution and collision handling

--- AOC2018/day13/day13.py
+++ AOC2018/day13/day13.py
@@ -14,10 +14,15 @@ class CartTurnDir(IntEnum):
 
 
 class Direction(Enum):
-    NORTH = (0, 1)
-    EAST = (1, 0)
-    SOUTH = (0, -1)
-    WEST = (-1, 0)
+    # format is y, x as used by numpy
+    NORTH = (-1, 0)
+    EAST = (0, 1)
+    SOUTH = (1, 0)
+    WEST = (0, -1)
+
+    @property
+    def coords(self):
+        return np.array(self.value)
 
 
 class Cart:
@@ -26,17 +31,64 @@ class Cart:
                'v': (Direction.SOUTH, '|'),
                '<': (Direction.WEST, '-')}
 
+    direction_changes = {(Direction.NORTH, '|'): Direction.NORTH,
+                         (Direction.NORTH, '/'): Direction.EAST,
+                         (Direction.NORTH, '\\'): Direction.WEST,
+                         (Direction.SOUTH, '|'): Direction.SOUTH,
+                         (Direction.SOUTH, '/'): Direction.WEST,
+                         (Direction.SOUTH, '\\'): Direction.EAST,
+                         (Direction.WEST, '-'): Direction.WEST,
+                         (Direction.WEST, '/'): Direction.SOUTH,
+                         (Direction.WEST, '\\'): Direction.NORTH,
+                         (Direction.EAST, '-'): Direction.EAST,
+                         (Direction.EAST, '/'): Direction.NORTH,
+                         (Direction.EAST, '\\'): Direction.SOUTH,
+                         }
+
+    symbol_finder = {i[0]: s for s, i in symbols.items()}
+
     def __init__(self, coords, symbol):
         self.coords = coords
         self.direction = self.symbols[symbol][0]
+
+    @property
+    def symbol(self):
+        return self.symbol_finder[self.direction]
+
+    @property
+    def reading_order(self):
+        return self.coords[0], self.coords[1]
+
+    def tick(self, map_):
+        self.move()
+        self.update_direction(map_)
+
+    def move(self):
+        self.coords += self.direction.coords
+
+    @property
+    def numpy_coords(self):
+        return tuple(self.coords)
+
+    def update_direction(self, map_):
+        self.direction = self.direction_changes[(self.direction, map_[self.numpy_coords])]
 
 
 class Track:
     def __init__(self, input_map: List[str]):
         self.map, self.carts = self.extract_carts(input_map)
+        self.collision = None
 
     def render_map(self):
         return [''.join(line) for line in self.map.tolist()]
+
+    def render(self):
+        map_ = self.map.copy()
+        for cart in self.carts:
+            map_[cart.numpy_coords] = cart.symbol
+        if self.collision:
+            map_[self.collision] = 'X'
+        return [''.join(line) for line in map_]
 
     @staticmethod
     def extract_carts(input_map: List[str]) -> Tuple[np.ndarray, List[Cart]]:
@@ -48,3 +100,21 @@ class Track:
         for symbol, info in Cart.symbols.items():
             map_[map_ == symbol] = info[1]
         return map_, carts
+
+    def tick(self):
+        sorted_carts = sorted(self.carts, key=lambda x: x.reading_order)
+        for cart in sorted_carts:
+            cart.tick(self.map)
+            self.check_for_collisions(cart)
+
+    def check_for_collisions(self, other):
+        for cart in self.carts:
+            if np.all(cart.coords == other.coords) and cart is not other:
+                self.collision = cart.numpy_coords
+                raise CollisionException(cart.coords)
+
+
+class CollisionException(Exception):
+    def __init__(self, coords, *args):
+        super().__init__(*args)
+        self.coords = coords
--- AOC2018/day13/test_day13.py
+++ AOC2018/day13/test_day13.py
@@ -1,4 +1,6 @@
-from AOC2018.day13.day13 import CartTurnDir, Track, Direction
+import re
+
+from AOC2018.day13.day13 import CartTurnDir, Track, Direction, CollisionException
 
 
 def test_dir_increment():
@@ -26,3 +28,30 @@ def test_simple_parse():
     assert track.carts[0].direction == Direction.SOUTH
     assert track.carts[1].coords == (5, 0)
     assert track.carts[1].direction == Direction.NORTH
+
+
+simple_evolution = '''|  |  |  |  |
+v  |  |  |  |
+|  v  v  |  |
+|  |  |  v  X
+|  |  ^  ^  |
+^  ^  |  |  |
+|  |  |  |  |'''
+
+
+def render_matches(track, strings):
+    return all([e == t for e, t in zip(track.render(), strings)])
+
+
+def test_simple_evolution():
+    lines = simple_evolution.split('\n')
+    simple_evolution_list = [re.split(r'\s{2,}', line) for line in lines]
+    initial_state, *states = list(zip(*simple_evolution_list))
+    track = Track(initial_state)
+    assert render_matches(track, initial_state)
+    for state in states[1::2]:
+        try:
+            track.tick()
+        except CollisionException:
+            pass
+        assert render_matches(track, state)
