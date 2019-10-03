000ef7d99135a21acd8e116bc593847087cc6318
Ed Rogers <etfrogers@hotmail.com>
2019-10-03 20:57:08 +0100

    2016 day 16 simplify checks

--- AOC2016/day18/day18.py
+++ AOC2016/day18/day18.py
@@ -17,12 +17,8 @@ class Room:
         new_row = list(SAFE * len(old_row))
         for pos in range(len(old_row)):
             left = SAFE if pos == 0 else old_row[pos-1]
-            centre = old_row[pos]
             right = SAFE if pos == len(old_row)-1 else old_row[pos+1]
-            if (left == centre == TRAP and right == SAFE) or \
-               (centre == right == TRAP and left == SAFE) or \
-               (left == TRAP and right == centre == SAFE) or \
-               (right == TRAP and left == centre == SAFE):
+            if left != right:
                 new_row[pos] = TRAP
 
         return ''.join(new_row)
@@ -38,6 +34,11 @@ def main():
 
     room = Room(first_row, n_rows)
     print('Part 1: Number of safe squares = ', room.n_safe_squares)
+
+    n_rows2 = 40000
+
+    room2 = Room(first_row, n_rows2)
+    print('Part 2: Number of safe squares = ', room2.n_safe_squares)
 
 
 if __name__ == '__main__':
