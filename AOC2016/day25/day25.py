2190731dafe799061ba0b7915a58fbf8476c7955
Ed Rogers <etfrogers@hotmail.com>
2019-10-10 19:44:27 +0100

    2016 day 25 part 1

--- AOC2016/day25/day25.py
+++ AOC2016/day25/day25.py
@@ -12,6 +12,8 @@ class AssemBunnyInterpreter25(AssemBunnyInterpreter23):
                                  'out': self.output_value}
         self.output = []
         self.output_limit = 1000000
+        self.n_cycles = 1
+        self.completed_cycles = 0
         self.abort_on_mismatch = abort_on_mismatch
         super(AssemBunnyInterpreter, self).__init__(instruction_list)
 
@@ -23,8 +25,12 @@ class AssemBunnyInterpreter25(AssemBunnyInterpreter23):
             return self.output
 
     def jnz(self, comparator, jump_size):
-        if comparator == '0':
-            print(self.register_string)
+        # if comparator == '0':
+        #     print(self.register_string)
+        if comparator == '1' and jump_size == '-21':
+            self.completed_cycles += 1
+            if self.completed_cycles == self.n_cycles:
+                return self.output
         super(AssemBunnyInterpreter25, self).jnz(comparator, jump_size)
 
 
@@ -33,64 +39,87 @@ def main():
         prog = file.readlines()
     interp = AssemBunnyInterpreter25(prog)
     for a in range(1, 100000):
-        print(f'Trying a={a}')
-        interp.registers['a'] = a
-        interp.execute(show_status=False)
-        if len(interp.output) == interp.output_limit:
-            print(f'Found a ={a} works for 1,000,000 outputs')
+        output = python_translation_opt(a, 10000000)
+        if all([v == i % 2 for i, v in enumerate(output)]):
+            print(f'Part 1: Found a = {a} works')
             break
-    # print('Part 1: Value of a is ', interp.registers['a'])
 
 
-def python_translation(a, n_output):
+# def python_translation(a, n_output):
+#     """ NOT WORKING """
+#     output = []
+#     d = a  # cpy a d
+#     c = 11  # cpy 11 c
+#     while True:
+#         b = 231  # cpy 231 b
+#         while True:
+#             d += 1  # inc d
+#             b -= 1  # dec b
+#             if b == 0:  # jnz b -2
+#                 break
+#         c -= 1  # dec c
+#         if c == 0:  # jnz c -5
+#             break
+#     while True:
+#         a = d  # cpy d a
+#         breakout = False
+#         while True:
+#             # jnz 0 0 - NOOP
+#             b = a  # cpy a b
+#             a = 0  # cpy 0 a
+#             while True:
+#                 c = 2  # cpy 2 c
+#                 a += 1  # inc a
+#                 while True:
+#                     if b == 0:  # jnz b 2
+#                         breakout = True
+#                         break  # jnz 1 6
+#                     b -= 1  # dec b
+#                     c -= 1  # dec c
+#                     if c == 0:  # jnz c -4
+#                         break
+#                 if breakout:
+#                     break
+#                 # jnz 1 -7
+#             # if b == 0:?
+#             b = 2  # cpy 2 b
+#             while True:
+#                 if c == 0:  # jnz c 2
+#                     break  # jnz 1 4
+#                 b -= 1  # dec b
+#                 c -= 1  # dec c
+#                 # jnz 1 -4
+#             # jnz 0 0 - NOOP
+#             output.append(b)  # out b
+#             if len(output) >= n_output:
+#                 return output
+#             if a == 0:  # jnz a -19
+#                 break
+#         # jnz 1 -21
+
+
+def python_translation_opt(a, n_output):
     output = []
     d = a  # cpy a d
     c = 11  # cpy 11 c
-    while True:
-        b = 231  # cpy 231 b
-        while True:
-            d += 1  # inc d
-            b -= 1  # dec b
-            if b == 0:  # jnz b -2
-                break
-        c -= 1  # dec c
-        if c == 0:  # jnz c -5
-            break
+    b = 231
+    d += b * c
     while True:
         a = d  # cpy d a
-        breakout = False
         while True:
             # jnz 0 0 - NOOP
             b = a  # cpy a b
-            a = 0  # cpy 0 a
-            while True:
-                c = 2  # cpy 2 c
-                a += 1  # inc a
-                while True:
-                    if b == 0:  # jnz b 2
-                        breakout = True
-                        break  # jnz 1 6
-                    b -= 1  # dec b
-                    c -= 1  # dec c
-                    if c == 0:  # jnz c -4
-                        break
-                if breakout:
-                    break
-                # jnz 1 -7
-            # if b == 0:?
+            a = b // 2
+            c = b % 2
+            if c == 0:
+                c = 2
             b = 2  # cpy 2 b
-            while True:
-                if c == 0:  # jnz c 2
-                    break  # jnz 1 4
-                b -= 1  # dec b
-                c -= 1  # dec c
-                # jnz 1 -4
-            # jnz 0 0 - NOOP
+            b -= c
             output.append(b)  # out b
             if len(output) >= n_output:
                 return output
             if a == 0:  # jnz a -19
-                break
+                return output
         # jnz 1 -21
 
 
--- AOC2016/day25/test_day25.py
+++ AOC2016/day25/test_day25.py
@@ -1,21 +1,41 @@
-from AOC2016.day25.day25 import AssemBunnyInterpreter25, python_translation
+from AOC2016.day25.day25 import AssemBunnyInterpreter25, python_translation, python_translation_opt
 
 
 def test_python_vs_interp():
-    for i in range(1, 7):
+    for i in range(1, 70):
         yield check_python_vs_interp, i
 
 
 def check_python_vs_interp(i):
-    n_output = 100
+    n_output = 1000
     with open('input.txt') as file:
         prog = file.readlines()
     interp = AssemBunnyInterpreter25(prog, abort_on_mismatch=False)
     interp.output_limit = n_output
+    interp.n_cycles = 1
     interp.registers['a'] = i
-    interp.execute(show_status=False)
-    python_output = python_translation(i, n_output)
-    assert all([p == i for p, i in zip(python_output, interp.output)])
+    interp_output = interp.execute(show_status=False)
+    # python_output = python_translation(i, n_output)
+    python_opt_output = python_translation_opt(i, n_output)
+    assert len(python_opt_output) == len(interp_output)
+    assert all([p == i for p, i in zip(python_opt_output, interp_output)])
+
+
+def test_cycling():
+    for i in range(1, 70):
+        yield check_cycling, i
+
+
+def check_cycling(i):
+    with open('input.txt') as file:
+        prog = file.readlines()
+    interp = AssemBunnyInterpreter25(prog, abort_on_mismatch=False)
+    interp.n_cycles = 2
+    interp.registers['a'] = i
+    interp_output = interp.execute(show_status=False)
+    length = len(interp_output)
+
+    assert all([p == i for p, i in zip(interp_output[:length//2], interp_output[length//2:])])
 
 
 # def test_part_1():
