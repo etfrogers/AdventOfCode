056d9ed51e08fdfad39a5c419dd192c89eba6097
Ed Rogers <etfrogers@hotmail.com>
2019-10-14 14:07:02 +0100

    2015 day 4 part 2

--- AOC2015/day4/day4.py
+++ AOC2015/day4/day4.py
@@ -7,7 +7,11 @@ def get_lowest_decimal(key, part2=False):
         hasher = hashlib.md5()
         hasher.update((key + str(i)).encode('ascii'))
         hash = hasher.hexdigest()
-        if hash[:5] == '00000':
+        if part2:
+            length = 6
+        else:
+            length = 5
+        if hash[:length] == '0'*length:
             return i
         i += 1
 
@@ -16,6 +20,9 @@ def main():
     key = 'bgvyzdsv'
     decimal = get_lowest_decimal(key)
     print(f'Part 1: Lowest integer is {decimal}')
+
+    decimal = get_lowest_decimal(key, part2=True)
+    print(f'Part 2: Lowest integer is {decimal}')
 
 
 if __name__ == '__main__':
