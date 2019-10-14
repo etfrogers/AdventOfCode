392d67b1e61e048dfa35197cab2d0894033fa52f
Ed Rogers <etfrogers@hotmail.com>
2019-10-14 11:16:28 +0100

    2015 day 3 part 2

--- AOC2015/day3/day3.py
+++ AOC2015/day3/day3.py
@@ -11,7 +11,13 @@ def get_number_of_unique_houses(houses):
     return len(set(houses))
 
 
-def get_visits(instructions: str):
+def get_visits(instructions: str, part2=False):
+    if part2:
+        santa_houses = get_visits(instructions[::2], part2=False)
+        robot_houses = get_visits(instructions[1::2], part2=False)
+        santa_houses.extend(robot_houses)
+        return santa_houses
+
     pos = Point(0, 0)
     visited = [pos.tuple]
     for instruction in instructions:
@@ -26,6 +32,10 @@ def main():
     houses = get_visits(instructions)
     unique_houses = get_number_of_unique_houses(houses)
     print(f'Part 1: Number of unique houses is {unique_houses}')
+
+    houses = get_visits(instructions, part2=True)
+    unique_houses = get_number_of_unique_houses(houses)
+    print(f'Part 2: Number of unique houses is {unique_houses}')
 
 
 if __name__ == '__main__':
--- AOC2015/day3/test_day3.py
+++ AOC2015/day3/test_day3.py
@@ -20,3 +20,24 @@ def test_3():
     houses = get_visits(instructions)
     n_houses = get_number_of_unique_houses(houses)
     assert n_houses == 2
+
+
+def test_part2_1():
+    instructions = '^v'
+    houses = get_visits(instructions, part2=True)
+    n_houses = get_number_of_unique_houses(houses)
+    assert n_houses == 3
+
+
+def test_part2_2():
+    instructions = '^>v<'
+    houses = get_visits(instructions, part2=True)
+    n_houses = get_number_of_unique_houses(houses)
+    assert n_houses == 3
+
+
+def test_part2_3():
+    instructions = '^v^v^v^v^v'
+    houses = get_visits(instructions, part2=True)
+    n_houses = get_number_of_unique_houses(houses)
+    assert n_houses == 11
