d578b4a60e3c0c8e4beeba6a4d10d3072491a09e
Ed Rogers <etfrogers@hotmail.com>
2019-10-16 13:29:53 +0100

    2015 day 6 part 2

--- AOC2015/day6/day6.py
+++ AOC2015/day6/day6.py
@@ -2,23 +2,35 @@ import numpy as np
 
 
 class Lights:
-    def __init__(self, size=1000):
+    def __init__(self, size=1000, part2=False):
         self.lights = np.zeros((size, size), dtype=int)
+        self.part2 = part2
 
     def process_instructions(self, instructions):
         for instruction in instructions:
             action, start_points, end_points = self.parse_instruction(instruction)
             x_slice = slice(start_points[0], end_points[0]+1)
             y_slice = slice(start_points[1], end_points[1]+1)
-            if action == 'on':
-                self.lights[x_slice, y_slice] = 1
-            elif action == 'off':
-                self.lights[x_slice, y_slice] = 0
-            elif action == 'toggle':
-                self.lights[x_slice, y_slice] = \
-                    abs(self.lights[x_slice, y_slice] - 1)
+            if self.part2:
+                if action == 'on':
+                    self.lights[x_slice, y_slice] += 1
+                elif action == 'off':
+                    self.lights[x_slice, y_slice] -= 1
+                    self.lights[self.lights < 0] = 0
+                elif action == 'toggle':
+                    self.lights[x_slice, y_slice] += 2
+                else:
+                    raise ValueError
             else:
-                raise ValueError
+                if action == 'on':
+                    self.lights[x_slice, y_slice] = 1
+                elif action == 'off':
+                    self.lights[x_slice, y_slice] = 0
+                elif action == 'toggle':
+                    self.lights[x_slice, y_slice] = \
+                        abs(self.lights[x_slice, y_slice] - 1)
+                else:
+                    raise ValueError
 
     @staticmethod
     def parse_instruction(instruction):
@@ -35,6 +47,10 @@ class Lights:
 
     @property
     def number_lit(self):
+        return np.sum(self.lights != 0)
+
+    @property
+    def total_brightness(self):
         return np.sum(self.lights)
 
 
@@ -44,6 +60,10 @@ def main():
     lights = Lights()
     lights.process_instructions(instructions)
     print(f'Part 1: Number of lit lights {lights.number_lit}')
+
+    lights = Lights(part2=True)
+    lights.process_instructions(instructions)
+    print(f'Part 2: Total brightness {lights.total_brightness}')
 
 
 if __name__ == '__main__':
--- AOC2015/day6/test_day6.py
+++ AOC2015/day6/test_day6.py
@@ -14,3 +14,30 @@ def test_1():
     instructions = ['turn off 499,499 through 500,500']
     lights.process_instructions(instructions)
     assert lights.number_lit == 1000000 - 1000 - 4
+
+
+def test_part_1():
+    with open('input.txt') as f:
+        instructions = f.readlines()
+    lights = Lights()
+    lights.process_instructions(instructions)
+    assert lights.number_lit == 400410
+
+
+def test_2():
+    lights = Lights(part2=True)
+    instructions = ['turn on 0,0 through 0,0']
+    lights.process_instructions(instructions)
+    assert lights.total_brightness == 1
+
+    instructions = ['toggle 0,0 through 999,999']
+    lights.process_instructions(instructions)
+    assert lights.total_brightness == 2000000 + 1
+
+
+def test_part_2():
+    with open('input.txt') as f:
+        instructions = f.readlines()
+    lights = Lights(part2=True)
+    lights.process_instructions(instructions)
+    assert lights.total_brightness == 15343601
