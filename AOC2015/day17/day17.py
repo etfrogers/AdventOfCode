bda7e03353ef49f9f9fc036ad43d5d11f0284342
Ed Rogers <etfrogers@hotmail.com>
2019-10-20 12:20:27 +0100

    2015 day 17 part 2

--- AOC2015/day17/day17.py
+++ AOC2015/day17/day17.py
@@ -14,8 +14,12 @@ def main():
         strings = f.readlines()
     sizes = [int(s.strip()) for s in strings]
     combos = get_combos(sizes, target_size)
-    print(f'Part 1: number of combinations is {len(combos)}')
+    print(f'Part 1: Number of combinations is {len(combos)}')
 
+    min_number = min([len(c) for c in combos])
+    print(f'Minimum number of containers is {min_number}')
+    min_combos = [c for c in combos if len(c) == min_number]
+    print(f'Part 2: Number of combinations with {min_number} containers is {len(min_combos)}')
 
 
 if __name__ == '__main__':
--- AOC2015/day17/test_day17.py
+++ AOC2015/day17/test_day17.py
@@ -12,3 +12,34 @@ def test_1():
     assert (5, 5, 15) in combos
     assert (5, 20) in combos
     assert combos.count((5, 20)) == 2
+
+
+def test_2():
+    target_size = 25
+    combos = get_combos(test_sizes, target_size)
+    min_number = min([len(c) for c in combos])
+    min_combos = [c for c in combos if len(c) == min_number]
+    assert len(min_combos) == 3
+
+
+def test_part1():
+    target_size = 150
+    with open('input.txt') as f:
+        strings = f.readlines()
+    sizes = [int(s.strip()) for s in strings]
+    combos = get_combos(sizes, target_size)
+    assert len(combos) == 1304
+
+
+def test_part2():
+    target_size = 150
+    with open('input.txt') as f:
+        strings = f.readlines()
+    sizes = [int(s.strip()) for s in strings]
+    combos = get_combos(sizes, target_size)
+
+    min_number = min([len(c) for c in combos])
+    assert min_number == 4
+    min_combos = [c for c in combos if len(c) == min_number]
+    assert len(min_combos) == 18
+
