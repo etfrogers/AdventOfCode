ab5fb10e96c9baa28995f6cb26d5d89dc4e78bf9
Ed Rogers <etfrogers@hotmail.com>
2019-11-24 15:54:12 +0000

    2015 day 20 part 2

--- AOC2015/day20/day20.py
+++ AOC2015/day20/day20.py
@@ -1,5 +1,5 @@
 import itertools
-from collections import defaultdict
+import numpy as np
 
 
 def factors(n):
@@ -13,48 +13,27 @@ def how_many_presents(n, part2=False):
     return 10*sum(factors(n))
 
 
-def how_many_presents_iter(limit=1000, part2=False):
-    working_elves = defaultdict(int)
-    if part2:
-        presents_per_elf = 11
-    else:
-        presents_per_elf = 10
-    for n in range(limit):
-        factors_ = factors(n)
-        if part2:
-            for f in factors_:
-                working_elves[f] += 1
-            for elf in list(working_elves.keys()):
-                if working_elves[elf] >= 51:
-                    del working_elves[elf]
-            factors_ = [f for f in factors_ if f in working_elves]
-        yield presents_per_elf * sum(factors_)
-
-
 def min_house(presents, print_=False, part2=False):
-    if part2:
-        n = 1
-        for n, p in enumerate(how_many_presents_iter(limit=1000000, part2=part2)):
-            if print_ and n % 1000 == 0:
-                print(f'{n}: {p}')
-            if p > presents:
-                break
-    else:
-        n = 1
-        while (p := how_many_presents(n)) < presents:
-            if print_ and n % 1000 == 0:
-                print(f'{n}: {p}')
-            n += 1
-    return n
+    n = presents // 10
+    houses = np.zeros((n, ))
+    for i in range(1, n):
+        if print_ and i % 1000 == 0:
+            print(i)
+        if part2:
+            houses[i:50*i+1:i] += 11 * i
+        else:
+            houses[i::i] += 10 * i
+    lowest_house = np.min(np.where(houses >= presents))
+    return lowest_house, houses
 
 
 def main():
     presents = 29000000
 
-    # n = min_house(presents, print_=True)
-    # print(f'Part 1: Minimum house with {presents} presents is {n}')
+    n, _ = min_house(presents, print_=False)
+    print(f'Part 1: Minimum house with {presents} presents is {n}')
 
-    n = min_house(presents, print_=True, part2=True)
+    n, _ = min_house(presents, print_=False, part2=True)
     print(f'Part 2: Minimum house with {presents} presents is {n}')
 
 
--- AOC2015/day20/test_day20.py
+++ AOC2015/day20/test_day20.py
@@ -1,6 +1,6 @@
 import pytest
 
-from AOC2015.day20.day20 import how_many_presents, min_house, how_many_presents_iter
+from AOC2015.day20.day20 import how_many_presents, min_house
 
 tests = [(1, 10),
          (2, 30),
@@ -19,12 +19,42 @@ def test_1(n, presents):
     assert presents == how_many_presents(n)
 
 
-def test_iter():
-    for n, p in enumerate(how_many_presents_iter(10000)):
-        assert p == how_many_presents(n)
+@pytest.mark.parametrize('h, p', tests)
+def test_n_presents_numpy(h, p):
+    max_presents = max([p for h, p in tests])
+    n, houses = min_house(max_presents)
+    assert houses[h] == p
+
+
+@pytest.mark.parametrize('h, p', tests)
+def test_n_presents_numpy_part2(h, p):
+    max_presents = max([p for h, p in tests])
+    n, houses = min_house(max_presents, part2=True)
+    assert houses[h] == 11 * p / 10
+
+
+@pytest.mark.parametrize('i', range(100))
+def test_n_presents_numpy_part1_vs_part2(i):
+    max_presents = 1000
+    n1, houses1 = min_house(max_presents, part2=False)
+    n2, houses2 = min_house(max_presents, part2=True)
+    adjustment = 0 if i <= 50 else 11
+    assert houses2[i] == (11 * houses1[i] / 10) - adjustment
+
+
+def test_small_presents():
+    presents = 150
+    n, _ = min_house(presents)
+    assert n == 8
 
 
 def test_part1():
     presents = 29000000
-    n = min_house(presents)
+    n, _ = min_house(presents)
     assert n == 665280
+
+
+def test_part2():
+    presents = 29000000
+    n, _ = min_house(presents, part2=True)
+    assert n == 705600
