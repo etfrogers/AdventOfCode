3d689276895a397cb3c776e4e711a6f861107773
Ed Rogers <etfrogers@hotmail.com>
2019-12-11 21:37:02 +0000

    2019 day 4 part 2

--- AOC2019/day4/day4.py
+++ AOC2019/day4/day4.py
@@ -1,5 +1,19 @@
-def two_digits_same(password):
-    return any([d == 0 for d in diff(password)])
+def two_digits_same(password, part2):
+    dp = diff(password)
+    if part2:
+        ddp = diff(dp)
+        ddp = [-1] + ddp + [-1]
+        candidates = [i for i in range(len(dp)) if dp[i] == 0]
+        if not candidates:
+            valid = False
+        else:
+            valid = False
+            for candidate in candidates:
+                if ddp[candidate] != 0 and ddp[candidate+1] != 0:
+                    valid = True
+        return valid
+    else:
+        return any([d == 0 for d in dp])
 
 
 def diff(password):
@@ -10,18 +24,21 @@ def non_decreasing(password):
     return all([d >= 0 for d in diff(password)])
 
 
-def is_valid(password):
-    return len(password) == 6 and two_digits_same(password) and non_decreasing(password)
+def is_valid(password, part2=False):
+    return len(password) == 6 and two_digits_same(password, part2) and non_decreasing(password)
 
 
 def main():
     input_ = (236491, 713787)
-    counter = 0
+    counter1 = counter2 = 0
     for i in range(input_[0], input_[1] + 1):
         password = str(i)
         if is_valid(password):
-            counter += 1
-    print(f'Number of valid passwords: {counter}')
+            counter1 += 1
+        if is_valid(password, part2=True):
+            counter2 += 1
+    print(f'Number of valid passwords part 1: {counter1}')
+    print(f'Number of valid passwords part 2: {counter2}')
 
 
 if __name__ == '__main__':
--- AOC2019/day4/test_day4.py
+++ AOC2019/day4/test_day4.py
@@ -7,7 +7,43 @@ cases = [('111111', True),
          ('123789', False)
          ]
 
+cases2 = [('112233', True),
+          ('123444', False),
+          ('111122', True),
+          ('111111', False),
+          ('123789', False),
+          ('112354', False),
+          ('112345', True),
+          ('113222', False),
+          ('112333', True),
+          ]
+
+input_ = (236491, 713787)
+
 
 @pytest.mark.parametrize('pw, valid', cases)
 def test_1(pw, valid):
     assert is_valid(pw) == valid
+
+
+def test_part1():
+    counter = 0
+    for i in range(input_[0], input_[1] + 1):
+        password = str(i)
+        if is_valid(password):
+            counter += 1
+    assert counter == 1169
+
+
+@pytest.mark.parametrize('pw, valid', cases2)
+def test_2(pw, valid):
+    assert is_valid(pw, part2=True) == valid
+
+
+def test_part2():
+    counter = 0
+    for i in range(input_[0], input_[1] + 1):
+        password = str(i)
+        if is_valid(password, part2=True):
+            counter += 1
+    assert counter == 757
