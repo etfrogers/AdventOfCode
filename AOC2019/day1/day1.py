5ceb92b382b40c5c59fe161c08622c9e64d86add
Ed Rogers <etfrogers@hotmail.com>
2019-12-11 23:17:07 +0000

    2019 day 1 part 2

--- AOC2019/day1/day1.py
+++ AOC2019/day1/day1.py
@@ -3,12 +3,23 @@ def fuel(mass):
     return (mass // 3) - 2
 
 
+def recursive_fuel(mass):
+    fuel_ = fuel(mass)
+    if fuel_ <= 0:
+        return 0
+    else:
+        return fuel_ + recursive_fuel(fuel_)
+
+
 def main():
     with open('input.txt') as f:
         masses = f.readlines()
     masses = [int(m.strip()) for m in masses]
     total_fuel = sum([fuel(mass) for mass in masses])
     print(f'Total fuel: {total_fuel}')
+
+    total_fuel_recursive = sum([recursive_fuel(mass) for mass in masses])
+    print(f'Fuel including fuel for fuel: {total_fuel_recursive}')
 
 
 if __name__ == '__main__':
--- AOC2019/day1/test_day1.py
+++ AOC2019/day1/test_day1.py
@@ -1,6 +1,6 @@
 import pytest
 
-from AOC2019.day1.day1 import fuel
+from AOC2019.day1.day1 import fuel, recursive_fuel
 
 cases = [(12, 2),
          (14, 2),
@@ -8,7 +8,33 @@ cases = [(12, 2),
          (100756, 33583),
          ]
 
+cases2 = [(14, 2),
+          (1969, 966),
+          (100756, 50346),
+          ]
+
 
 @pytest.mark.parametrize('mass, exp_fuel', cases)
 def test_1(mass, exp_fuel):
     assert fuel(mass) == exp_fuel
+
+
+def test_part1():
+    with open('input.txt') as f:
+        masses = f.readlines()
+    masses = [int(m.strip()) for m in masses]
+    total_fuel = sum([fuel(mass) for mass in masses])
+    assert total_fuel == 3303995
+
+
+@pytest.mark.parametrize('mass, exp_fuel', cases2)
+def test_2(mass, exp_fuel):
+    assert recursive_fuel(mass) == exp_fuel
+
+
+def test_part2():
+    with open('input.txt') as f:
+        masses = f.readlines()
+    masses = [int(m.strip()) for m in masses]
+    total_fuel_recursive = sum([recursive_fuel(mass) for mass in masses])
+    assert total_fuel_recursive == 4953118
