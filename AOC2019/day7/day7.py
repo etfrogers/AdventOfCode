77afa452d55c4dcfd6686740704698b5190d3b9f
Ed Rogers <etfrogers@hotmail.com>
2019-12-14 17:45:55 +0000

    2019 day 7 part 2

--- AOC2019/day7/day7.py
+++ AOC2019/day7/day7.py
@@ -3,20 +3,62 @@ import itertools
 from AOC2019.day5.day5 import IntCodeComputer2
 
 
-def optimise_phases(instructions):
+class AsyncIntCodeComputer(IntCodeComputer2):
+    def __init__(self, instructions, input_=None):
+        super().__init__(instructions, input_)
+        self.paused = False
+
+    def pause(self):
+        self.paused = True
+
+    def resume(self):
+        self.paused = False
+        self.execute()
+
+    def input(self, modes):
+        if len(self.input_data) == 0:
+            self.pause()
+            return
+        super().input(modes)
+
+    def execute(self):
+        while not (self.finished or self.paused):
+            opcode_, modes = self.get_opcode()
+            opcode_(modes)
+
+
+def optimise_phases(instructions, use_feedback=False):
     best_thrust = 0
     best_phases = None
-    for phases in itertools.permutations(range(5)):
-        starting_thrust = 0
-        for phase in phases:
-            comp = IntCodeComputer2(instructions, [phase, starting_thrust])
-            comp.execute()
-            assert len(comp.output_data) == 1
-            starting_thrust = comp.output_data[0]
-        if starting_thrust > best_thrust:
-            best_thrust = starting_thrust
+    if use_feedback:
+        possible_phases = range(5, 10)
+    else:
+        possible_phases = range(5)
+    for phases in itertools.permutations(possible_phases):
+        current_thrust = 0
+        if use_feedback:
+            current_thrust = run_feedback_amplifiers(instructions, phases)
+        else:
+            for phase in phases:
+                comp = IntCodeComputer2(instructions, [phase, current_thrust])
+                comp.execute()
+                assert len(comp.output_data) == 1
+                current_thrust = comp.output_data[0]
+        if current_thrust > best_thrust:
+            best_thrust = current_thrust
             best_phases = tuple(phases)
     return best_thrust, best_phases
+
+
+def run_feedback_amplifiers(instructions, phases):
+    amps = [AsyncIntCodeComputer(instructions, [phase]) for phase in phases]
+    current_thrust = 0
+    while not all([amp.finished for amp in amps]):
+        for amp in amps:
+            amp.input_data.append(current_thrust)
+            amp.resume()
+            current_thrust = amp.output_data[0]
+    return current_thrust
 
 
 def main():
@@ -24,8 +66,10 @@ def main():
         instructions = f.readline()
 
     thrust, phases = optimise_phases(instructions)
-
     print(f'Optimal thrust: {thrust} - {phases}')
+
+    thrust, phases = optimise_phases(instructions, use_feedback=True)
+    print(f'Optimal thrust with feedback: {thrust} - {phases}')
 
 
 if __name__ == '__main__':
--- AOC2019/day7/test_day7.py
+++ AOC2019/day7/test_day7.py
@@ -8,6 +8,13 @@ cases = [('3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0', 43210, (4, 3, 2, 1, 
           65210, (1, 0, 4, 3, 2)),
          ]
 
+cases2 = [('3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5',
+          139629729, (9, 8, 7, 6, 5)),
+          (('3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,' +
+            '-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,' +
+            '53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10'), 18216, (9, 7, 8, 5, 6))
+          ]
+
 
 @pytest.mark.parametrize('instructions, expected_thrust, expected_phases', cases)
 def test_1(instructions, expected_thrust, expected_phases):
@@ -22,3 +29,18 @@ def test_part1():
     thrust, phases = optimise_phases(instructions)
     assert thrust == 844468
     assert phases == (0, 2, 3, 4, 1)
+
+
+@pytest.mark.parametrize('instructions, expected_thrust, expected_phases', cases2)
+def test_2(instructions, expected_thrust, expected_phases):
+    thrust, phases = optimise_phases(instructions, use_feedback=True)
+    assert thrust == expected_thrust
+    assert phases == expected_phases
+
+
+def test_part2():
+    with open('input.txt') as f:
+        instructions = f.readline()
+    thrust, phases = optimise_phases(instructions, use_feedback=True)
+    assert thrust == 4215746
+    assert phases == (6, 5, 8, 7, 9)
