6066b63009c4ce180d618b319f9f9c717ef5d0ee
Ed Rogers <etfrogers@hotmail.com>
2023-12-19 05:45:35 +0000

    Day 1, part 2 incorrect solution

--- AOC2023/day1/day1.go
+++ AOC2023/day1/day1.go
@@ -8,18 +8,47 @@ import (
 	"strings"
 )
 
+var DIGIT_MAPPING = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+}
+
 func GetCalibration(data string) int {
 	lines := strings.Split(data, "\n")
 	digit_pattern, _ := regexp.Compile(`\d`)
 	total := 0
 	for _, line := range lines {
-		digits := digit_pattern.FindAll([]byte(line), -1)
-		str_number := []byte{digits[0][0], digits[len(digits)-1][0]}
-		value, err := strconv.Atoi(string(str_number))
+		digits := digit_pattern.FindAllString(line, -1)
+		str_number := digits[0] + digits[len(digits)-1]
+		fmt.Println(line, str_number)
+		value, err := strconv.Atoi(str_number)
 		check(err)
 		total += value
 	}
+	fmt.Println("")
 	return total
+}
+
+func ConvertTextToDigit(data string) string {
+	new_data := data
+	var keys []string
+	i := 0
+	for k := range DIGIT_MAPPING {
+		keys = append(keys, "("+k+")")
+		i++
+	}
+	re_str := strings.Join(keys, "|")
+	re, err := regexp.Compile(re_str)
+	check(err)
+	new_data = re.ReplaceAllStringFunc(new_data, func(k string) string { return DIGIT_MAPPING[k] })
+	return new_data
 }
 
 func check(e error) {
@@ -32,8 +61,11 @@ func main() {
 	doc_bytes, err := os.ReadFile("input.txt")
 	var document string = string(doc_bytes[:])
 	check(err)
-	// fmt.Print(string(document))
 	calibration_code := GetCalibration(document)
 	fmt.Printf("Day 1, Part 1: %d\n", calibration_code)
+
+	converted_doc := ConvertTextToDigit(document)
+	part2_code := GetCalibration(converted_doc)
+	fmt.Printf("Day 1, Part 2: %d\n", part2_code)
 
 }
--- AOC2023/day1/day1_test.go
+++ AOC2023/day1/day1_test.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 // Test the first test case give the part 1 result
-func TestHelloName(t *testing.T) {
+func TestPart1TestCase(t *testing.T) {
 	data := `1abc2
  pqr3stu8vwx
  a1b2c3d4e5f
@@ -17,13 +18,92 @@ func TestHelloName(t *testing.T) {
 	}
 }
 
-// TestHelloEmpty calls greetings.Hello with an empty string,
-// checking for an error.
-/*
-func TestHelloEmpty(t *testing.T) {
-    msg, err := Hello("")
-    if msg != "" || err == nil {
-        t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
-    }
+func TestPart1(t *testing.T) {
+	doc_bytes, err := os.ReadFile("input.txt")
+	var document string = string(doc_bytes[:])
+	check(err)
+	calibration_code := GetCalibration(document)
+	expected := 53651
+	if calibration_code != expected {
+		t.Fatalf("Unexpected value %v. Expected %d", calibration_code, expected)
+	}
 }
-*/
+
+func TestPart2TestCase(t *testing.T) {
+	data := `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
+	expected := 281
+
+	data = ConvertTextToDigit(data)
+	calibration_code := GetCalibration(data)
+	if calibration_code != expected {
+		t.Fatalf("Unexpected value %v. Expected %d", calibration_code, expected)
+	}
+}
+
+func TestPart2CaseSeparate(t *testing.T) {
+	testCases := []struct {
+		line string
+		want int
+	}{
+		{"two1nine", 29},
+		{"eightwothree", 83},
+		{"abcone2threexyz", 13},
+		{"xtwone3four", 24},
+		{"4nineeightseven2", 42},
+		{"zoneight234", 14},
+		{"7pqrstsixteen", 76},
+		{"eighthree", 83},
+		{"sevenine", 79},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.line, func(t *testing.T) {
+			data := ConvertTextToDigit(tc.line)
+			calibration_code := GetCalibration(data)
+			if calibration_code != tc.want {
+				t.Fatalf("Unexpected value %v. Expected %d", calibration_code, tc.want)
+			}
+		})
+	}
+}
+
+func TestConversion(t *testing.T) {
+	testCases := []struct {
+		line string
+		want string
+	}{
+		{"two1nine", "219"},
+		{"eightwothree", "8wo3"},
+		{"abcone2threexyz", "abc123xyz"},
+		{"xtwone3four", "x2ne34"},
+		{"4nineeightseven2", "49872"},
+		{"zoneight234", "z1ight234"},
+		{"7pqrstsixteen", "7pqrst6teen"},
+		{"one", "1"},
+		{"two", "2"},
+		{"three", "3"},
+		{"four", "4"},
+		{"five", "5"},
+		{"six", "6"},
+		{"seven", "7"},
+		{"eight", "8"},
+		{"nine", "9"},
+		{"onetwoonetwo", "1212"},
+		{"one1one1", "1111"},
+		{"eighthree", "83"},
+		{"sevenine", "79"},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.line, func(t *testing.T) {
+			converted := ConvertTextToDigit(tc.line)
+			if converted != tc.want {
+				t.Fatalf("Unexpected value %s. Expected %s", converted, tc.want)
+			}
+		})
+	}
+}
