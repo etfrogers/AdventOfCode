8108f902b21c7060ddf3358464399aa5f493663d
Ed Rogers <etfrogers@hotmail.com>
2023-12-23 06:57:55 +0000

    Add ReadINoput and PowInts to utils

--- AOC2023/utils/utils.go
+++ AOC2023/utils/utils.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func Map[T, V any](ts []T, fn func(T) V) []V {
 	result := make([]V, len(ts))
 	for i, t := range ts {
@@ -12,4 +18,28 @@ func Check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func ReadInput() []string {
+	doc, err := os.ReadFile("input.txt")
+	Check(err)
+	lines := strings.Split(string(doc), "\n")
+	return lines
+}
+
+func PowInts(x, n int) int {
+	if n < 0 {
+		panic(fmt.Errorf("n must be >= 0"))
+	}
+	if n == 0 {
+		return 1
+	}
+	if n == 1 {
+		return x
+	}
+	y := PowInts(x, n/2)
+	if n%2 == 0 {
+		return y * y
+	}
+	return x * y * y
 }
