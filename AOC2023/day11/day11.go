efc86dd530fa6fb75fef686783f5906f665b96ba
Ed Rogers <etfrogers@hotmail.com>
2024-01-05 10:03:28 +0000

    Day 11 Part 2

--- AOC2023/day11/day11.go
+++ AOC2023/day11/day11.go
@@ -2,32 +2,38 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"slices"
 	"strings"
 	"utils"
+	"utils/set"
 )
 
-type SpaceImage [][]string
+type SpaceImage struct {
+	data            [][]string
+	expX, expY      set.Set[int]
+	expansionFactor int
+}
 type Galaxy struct {
 	x, y int
 }
 
 func (im *SpaceImage) Size() (int, int) {
-	return len(*im), len((*im)[0])
+	return len(im.data), len((im.data)[0])
 }
 
-func NewSpaceImage(lines []string) SpaceImage {
+func NewSpaceImage(lines []string, expansion int) SpaceImage {
 	arr := make([][]string, len(lines))
 	for i, line := range lines {
 		arr[i] = strings.Split(line, "")
 	}
-	return SpaceImage(arr)
+	im := SpaceImage{data: arr, expansionFactor: expansion, expX: *set.New[int](), expY: *set.New[int]()}
+	im.Expand()
+	return im
 }
 
 func (im *SpaceImage) colAllDots(x int) bool {
-	for y := range *im {
-		if (*im)[y][x] != "." {
+	for y := range im.data {
+		if im.data[y][x] != "." {
 			return false
 		}
 	}
@@ -35,7 +41,7 @@ func (im *SpaceImage) colAllDots(x int) bool {
 }
 
 func (im *SpaceImage) rowAllDots(y int) bool {
-	return allDots(&(*im)[y])
+	return allDots(&im.data[y])
 }
 
 func allDots(s *[]string) bool {
@@ -57,33 +63,51 @@ func fullSlice[T ~[]E, E any](n int, elem E) T {
 }
 
 func (im *SpaceImage) InsertColDots(x int) {
-	for i := range *im {
-		(*im)[i] = slices.Insert((*im)[i], x, ".")
+	for i := range im.data {
+		im.data[i] = slices.Insert(im.data[i], x, ".")
 	}
 }
 
 func (im *SpaceImage) Expand() {
-	_, rowLen := im.Size()
 	//insert rows first (easiest)
-	for y := 0; y < len(*im); y++ {
+	for y := 0; y < len(im.data); y++ {
 		if im.rowAllDots(y) {
-			newRow := fullSlice[[]string, string](rowLen, ".")
-			*im = slices.Insert((*im), y, newRow)
-			y++
+			im.expY.Add(y)
 		}
 	}
 
-	for x := 0; x < len((*im)[0]); x++ {
+	for x := 0; x < len(im.data[0]); x++ {
 		if im.colAllDots(x) {
-			im.InsertColDots(x)
-			x++
+			im.expX.Add(x)
 		}
 	}
 }
 
 func (im *SpaceImage) String() string {
-	arr := make([]string, len(*im))
-	for i, chars := range *im {
+
+	_, rowLen := im.Size()
+	// //insert rows first (easiest)
+	insertInd := 0
+	for y := range im.data {
+		if im.expY.Contains(y) {
+			newRow := fullSlice[[]string, string](rowLen, ".")
+			im.data = slices.Insert(im.data, insertInd, newRow)
+			insertInd++
+		}
+		insertInd++
+	}
+
+	insertInd = 0
+	for x := range im.data[0] {
+		if im.expX.Contains(x) {
+			im.InsertColDots(insertInd)
+			insertInd++
+		}
+		insertInd++
+	}
+
+	arr := make([]string, len(im.data))
+	for i, chars := range im.data {
 		arr[i] = strings.Join(chars, "")
 	}
 	return strings.Join(arr, "\n")
@@ -91,9 +115,9 @@ func (im *SpaceImage) String() string {
 
 func (im *SpaceImage) FindGalaxies() []Galaxy {
 	galaxies := []Galaxy{}
-	for y := range *im {
-		for x := range (*im)[0] {
-			if (*im)[y][x] == "#" {
+	for y := range im.data {
+		for x := range im.data[0] {
+			if im.data[y][x] == "#" {
 				galaxies = append(galaxies, Galaxy{x, y})
 			}
 		}
@@ -106,15 +130,33 @@ func (im *SpaceImage) GalaxyDistances() []int {
 	dists := []int{}
 	for i := range galaxies {
 		for j := i + 1; j < len(galaxies); j++ {
-			dist := FindDistance(galaxies[i], galaxies[j])
+			dist := im.FindDistance(galaxies[i], galaxies[j])
 			dists = append(dists, dist)
 		}
 	}
 	return dists
 }
 
-func FindDistance(g1, g2 Galaxy) int {
-	return int(math.Abs(float64(g1.x-g2.x)) + math.Abs(float64(g1.y-g2.y)))
+func minmax(x, y int) (int, int) {
+	return min(x, y), max(x, y)
+}
+
+func (im *SpaceImage) FindDistance(g1, g2 Galaxy) int {
+	nExpansions := 0
+	minX, maxX := minmax(g1.x, g2.x)
+	for x := minX; x <= maxX; x++ {
+		if im.expX.Contains(x) {
+			nExpansions++
+		}
+	}
+	minY, maxY := minmax(g1.y, g2.y)
+	for y := minY; y <= maxY; y++ {
+		if im.expY.Contains(y) {
+			nExpansions++
+		}
+	}
+	baseDist := maxX - minX + maxY - minY
+	return baseDist + nExpansions*(im.expansionFactor-1)
 }
 
 func (im *SpaceImage) SumGalaxyDistances() int {
@@ -123,8 +165,11 @@ func (im *SpaceImage) SumGalaxyDistances() int {
 
 func main() {
 	lines := utils.ReadInput()
-	im := NewSpaceImage(lines)
-	im.Expand()
-	part1Answer := im.SumGalaxyDistances()
+	expansion := 2
+	im1 := NewSpaceImage(lines, expansion)
+	part1Answer := im1.SumGalaxyDistances()
 	fmt.Printf("Day 11, Part 1 answer: %d\n", part1Answer)
+	im2 := NewSpaceImage(lines, 1_000_000)
+	part2Answer := im2.SumGalaxyDistances()
+	fmt.Printf("Day 11, Part 2 answer: %d\n", part2Answer)
 }
--- AOC2023/day11/day11_test.go
+++ AOC2023/day11/day11_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"strings"
 	"testing"
 	"utils"
@@ -38,26 +39,23 @@ var expanded string = `....#........
 func TestPart1(t *testing.T) {
 	expected := 10231178
 	lines := utils.ReadInput()
-	im := NewSpaceImage(lines)
-	im.Expand()
+	im := NewSpaceImage(lines, 2)
 	part1Answer := im.SumGalaxyDistances()
 	assert.Equal(t, expected, part1Answer)
 }
 
 func TestExpansion(t *testing.T) {
-	im := NewSpaceImage(testLines)
-	im.Expand()
+	im := NewSpaceImage(testLines, 2)
 	assert.Equal(t, expanded, im.String())
 }
 
 func TestFindGalaxies(t *testing.T) {
-	im := NewSpaceImage(testLines)
-	im.Expand()
+	im := NewSpaceImage(testLines, 2)
 	galaxies := im.FindGalaxies()
 	assert.Equal(t, len(galaxies), 9)
-	assert.Equal(t, Galaxy{4, 0}, galaxies[0])
+	assert.Equal(t, Galaxy{3, 0}, galaxies[0])
 	assert.Equal(t, Galaxy{0, 2}, galaxies[2])
-	assert.Equal(t, Galaxy{12, 7}, galaxies[5])
+	assert.Equal(t, Galaxy{9, 6}, galaxies[5])
 }
 
 func TestDistances(t *testing.T) {
@@ -69,22 +67,48 @@ func TestDistances(t *testing.T) {
 		{1, 7, 15},
 		{3, 6, 17},
 		{8, 9, 5},
+		{1, 4, 9},
+		{3, 7, 17},
+		{4, 5, 8},
+		{5, 4, 8},
 	}
-	im := NewSpaceImage(testLines)
-	im.Expand()
+	im := NewSpaceImage(testLines, 2)
 	galaxies := im.FindGalaxies()
 	for _, tc := range testCases {
 		t.Run(fmt.Sprint(tc), func(t *testing.T) {
 			g1, g2 := galaxies[tc.i1-1], galaxies[tc.i2-1]
-			assert.Equal(t, tc.dist, FindDistance(g1, g2))
+			assert.Equal(t, tc.dist, im.FindDistance(g1, g2))
 		})
 	}
 }
 
-func TestSumDists(t *testing.T) {
-	im := NewSpaceImage(testLines)
-	im.Expand()
-	expected := 374
-	assert.Equal(t, expected, im.SumGalaxyDistances())
+func TestExpLocations(t *testing.T) {
+	im := NewSpaceImage(testLines, 2)
+	expectedLocsX := []int{2, 5, 8}
+	expectedLocsY := []int{3, 7}
 
+	actualX := im.expX.Items()
+	slices.Sort(actualX)
+	assert.Equal(t, expectedLocsX, actualX)
+
+	actualY := im.expY.Items()
+	slices.Sort(actualY)
+	assert.Equal(t, expectedLocsY, actualY)
+}
+
+func TestSumDists(t *testing.T) {
+	testCases := []struct {
+		expansion   int
+		expectedSum int
+	}{
+		{2, 374},
+		{10, 1030},
+		{100, 8410},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprint(tc), func(t *testing.T) {
+			im := NewSpaceImage(testLines, tc.expansion)
+			assert.Equal(t, tc.expectedSum, im.SumGalaxyDistances())
+		})
+	}
 }
