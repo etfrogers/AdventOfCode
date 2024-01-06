f359b27ddcf1563f7e4d42e8fc6005597db26cec
Ed Rogers <etfrogers@hotmail.com>
2024-01-06 19:27:13 +0000

    Day 15 Part 2

--- AOC2023/day15/day15.go
+++ AOC2023/day15/day15.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 	"utils"
+
+	orderedmap "github.com/wk8/go-ordered-map"
 )
 
 func Hash(s string) int {
 	hash := 0
-	for _, r := range []rune(s) {
+	for _, r := range s {
 		hash += int(r)
 		hash *= 17
 		hash %= 256
@@ -25,8 +28,72 @@ func Checksum(csv string) int {
 	return utils.Sum(HashSlice(csv))
 }
 
+var cmdRe = regexp.MustCompile(`([a-z]*)([=-])(\d?)`)
+
+type Boxes []orderedmap.OrderedMap
+type Lens struct {
+	box   int
+	slot  int
+	label string
+	lens  int
+}
+
+func FollowManual(csv string) Boxes {
+	boxes := make(Boxes, 256)
+	for i := range boxes {
+		boxes[i] = *orderedmap.New()
+	}
+	commands := strings.Split(csv, ",")
+	for _, cmd := range commands {
+		tokens := cmdRe.FindStringSubmatch(cmd)
+		label := tokens[1]
+		op := tokens[2]
+		box_ind := Hash(label)
+		switch op {
+		case "=":
+			lens := utils.AtoiError(tokens[3])
+			boxes[box_ind].Set(label, lens)
+		case "-":
+			boxes[box_ind].Delete(label)
+		}
+	}
+	return boxes
+}
+
+func GetLenses(boxes Boxes) []Lens {
+	lenses := make([]Lens, 0, 256)
+	for box_ind, box := range boxes {
+		slot := 1
+		for pair := box.Oldest(); pair != nil; pair = pair.Next() {
+			lens := Lens{box: box_ind, slot: slot, label: pair.Key.(string), lens: pair.Value.(int)}
+			lenses = append(lenses, lens)
+			slot++
+		}
+	}
+	return lenses
+}
+
+func (l Lens) FocusingPower() int {
+	return (l.box + 1) * l.slot * l.lens
+}
+
+func FocusingPowers(boxes Boxes) []int {
+	lenses := GetLenses(boxes)
+	powers := utils.Map(lenses, func(l Lens) int { return l.FocusingPower() })
+	return powers
+}
+
+func TotalPower(boxes Boxes) int {
+	return utils.Sum(FocusingPowers(boxes))
+}
+
 func main() {
 	input := utils.ReadInput()[0]
 	part1Answer := Checksum(input)
 	fmt.Printf("Day 15, Part 1 answer: %d\n", part1Answer)
+
+	boxes := FollowManual(input)
+	part2Answer := TotalPower(boxes)
+	fmt.Printf("Day 15, Part 2 answer: %d\n", part2Answer)
+
 }
--- AOC2023/day15/day15_test.go
+++ AOC2023/day15/day15_test.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"strings"
 	"testing"
+	"utils"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -10,8 +10,6 @@ import (
 var testCase string = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
 
 var expectedHashes []int = []int{30, 253, 97, 47, 14, 180, 9, 197, 48, 214, 231}
-
-var testLines []string = strings.Split(testCase, "\n")
 
 func TestHash(t *testing.T) {
 	assert.Equal(t, Hash("HASH"), 52)
@@ -25,4 +23,42 @@ func TestCaseHashes(t *testing.T) {
 func TestChecksum(t *testing.T) {
 	expected := 1320
 	assert.Equal(t, expected, Checksum(testCase))
+}
+
+func TestLenses(t *testing.T) {
+	expected := []Lens{
+		{0, 1, "rn", 1},
+		{0, 2, "cm", 2},
+		{3, 1, "ot", 7},
+		{3, 2, "ab", 5},
+		{3, 3, "pc", 6},
+	}
+	boxes := FollowManual(testCase)
+	lenses := GetLenses(boxes)
+	assert.Equal(t, expected, lenses)
+}
+
+func TestPowers(t *testing.T) {
+	expected := []int{1, 4, 28, 40, 72}
+	boxes := FollowManual(testCase)
+	powers := FocusingPowers(boxes)
+	assert.Equal(t, powers, expected)
+}
+
+func TestTotalPower(t *testing.T) {
+	boxes := FollowManual(testCase)
+	assert.Equal(t, 145, TotalPower(boxes))
+}
+
+func TestPart1(t *testing.T) {
+	input := utils.ReadInput()[0]
+	part1Answer := Checksum(input)
+	assert.Equal(t, 507291, part1Answer)
+}
+
+func TestPart2(t *testing.T) {
+	input := utils.ReadInput()[0]
+	boxes := FollowManual(input)
+	part2Answer := TotalPower(boxes)
+	assert.Equal(t, 296921, part2Answer)
 }
