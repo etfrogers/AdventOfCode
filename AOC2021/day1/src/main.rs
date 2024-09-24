54c7947381b1b7b29581366066cb57426a03e8af
Ed Rogers <etfrogers@hotmail.com>
2024-09-24 23:16:15 +0100

    Day 1 Part 2

--- AOC2021/day1/src/main.rs
+++ AOC2021/day1/src/main.rs
@@ -4,12 +4,23 @@ fn main() {
     let input = fs::read_to_string("day1/src/input.txt")
         .expect("Should have been able to read the file");
     let data = parse_input(&input);
-    let day1 = n_increases(data);
+    let day1 = n_increases(&data);
 
-    println!("Day 1 answer: {}", day1)
+    println!("Day 1 answer: {}", day1);
+
+    let windowed = windowed_sum(3, &data);
+    let day2 = n_increases(&windowed);
+    println!("Day 2 answer: {}", day2);
+
 }
 
-fn n_increases(data: Vec<i32>) -> i32 {
+fn windowed_sum(size: usize, data: &Vec<i32>) -> Vec<i32> {
+    data.windows(size)
+        .map(|x| x.into_iter().sum())
+        .collect()
+}
+
+fn n_increases(data: &Vec<i32>) -> i32 {
     let mut n = 0;
     let mut prev = &data[0];
     for entry in &data[1..] {
@@ -29,15 +40,54 @@ fn parse_input(input: &str) -> Vec<i32> {
 
 #[cfg(test)]
 mod test {
+    use std::env;
+
     use super::*;
 
     #[test]
     fn test_n_increases() {
         let data = parse_input(TEST_1);
-        let result = n_increases(data);
+        let result = n_increases(&data);
         assert_eq!(result, 7);
     }
 
+    #[test]
+    fn test_sliding_window() {
+        let data = parse_input(TEST_1);
+        let result = windowed_sum(3, &data);
+        assert_eq!(result[0], 607);
+        assert_eq!(result[1], 618);
+        assert_eq!(*(result.last().expect("empty list")), 269+260+263);
+    }
+
+    #[test]
+    fn test_sample_part2(){
+        let data = parse_input(TEST_1);
+        let result = n_increases(&windowed_sum(3, &data));
+        assert_eq!(result, 5)
+    }
+
+    #[test]
+    fn test_part1() {
+        println!("{:?}", env::current_dir());
+        let input = fs::read_to_string("src/input.txt")
+        .expect("Should have been able to read the file");
+        let data = parse_input(&input);
+        let day1 = n_increases(&data);
+        assert_eq!(day1, 1529)
+    }
+
+    #[test]
+    fn test_part2() {
+        println!("{:?}", env::current_dir());
+        let input = fs::read_to_string("src/input.txt")
+        .expect("Should have been able to read the file");
+        let data = parse_input(&input);
+        let windowed = windowed_sum(3, &data);
+        let day2 = n_increases(&windowed);
+        assert_eq!(day2, 1567)
+    }
+    
     const TEST_1: &str = "199
 200
 208
