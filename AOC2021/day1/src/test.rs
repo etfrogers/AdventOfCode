8d728a7073db705530841f4f8351cec8ec1933a9
Ed Rogers <etfrogers@hotmail.com>
2024-09-27 22:10:06 +0100

    Move day1 test code to separate file

---
+++ AOC2021/day1/src/test.rs
@@ -0,0 +1,52 @@
+use super::*;
+
+#[test]
+fn test_n_increases() {
+    let data = parse_input(utils::string_input_lines(TEST_1));
+    let result = n_increases(&data);
+    assert_eq!(result, 7);
+}
+
+#[test]
+fn test_sliding_window() {
+    let data = parse_input(utils::string_input_lines(TEST_1));
+    let result = windowed_sum(3, &data);
+    assert_eq!(result[0], 607);
+    assert_eq!(result[1], 618);
+    assert_eq!(*(result.last().expect("empty list")), 269+260+263);
+}
+
+#[test]
+fn test_sample_part2(){
+    let data = parse_input(utils::string_input_lines(TEST_1));
+    let result = n_increases(&windowed_sum(3, &data));
+    assert_eq!(result, 5)
+}
+
+#[test]
+fn test_part1() {
+    let input = utils::input_lines(1);
+    let data = parse_input(input);
+    let day1 = n_increases(&data);
+    assert_eq!(day1, 1529)
+}
+
+#[test]
+fn test_part2() {
+    let input = utils::input_lines(1);
+    let data = parse_input(input);
+    let windowed = windowed_sum(3, &data);
+    let day2 = n_increases(&windowed);
+    assert_eq!(day2, 1567)
+}
+
+const TEST_1: &str = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
