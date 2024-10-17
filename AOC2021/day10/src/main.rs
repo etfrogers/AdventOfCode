317bbb62e4dcaf81a9222ee117dacc688ca89e89
Ed Rogers <etfrogers@hotmail.com>
2024-10-17 21:46:09 +0100

    Day 10 Parts 1 and 2

---
+++ AOC2021/day10/src/main.rs
@@ -0,0 +1,99 @@
+use std::collections::{HashMap, HashSet};
+
+use lazy_static::lazy_static;
+use utils;
+
+enum LineState {
+    Corrupted(char),
+    Complete,
+    Incomplete(Vec<char>),
+}
+
+lazy_static! {
+    static ref PAIRS: HashMap<char, char> =
+        HashMap::from([('{', '}'), ('(', ')'), ('[', ']'), ('<', '>')]);
+}
+
+lazy_static! {
+    static ref CORRUPTION_SCORES: HashMap<char, u32> =
+        HashMap::from([(')', 3), (']', 57), ('}', 1197), ('>', 25137),]);
+}
+
+lazy_static! {
+    static ref COMPLETION_SCORES: HashMap<char, u64> =
+        HashMap::from([(')', 1), (']', 2), ('}', 3), ('>', 4),]);
+}
+
+fn process_line(line: &String) -> LineState {
+    let mut stack = Vec::with_capacity(line.len());
+    let openers: HashSet<_> = PAIRS.keys().collect();
+    let closers: HashSet<_> = PAIRS.values().collect();
+    for c in line.chars() {
+        match c {
+            a if openers.contains(&a) => stack.push(c),
+            a if closers.contains(&a) => {
+                let matcher = stack.pop().unwrap();
+                if c != PAIRS[&matcher] {
+                    return LineState::Corrupted(c);
+                }
+            }
+            _ => panic!("unexpected char: {}", c),
+        }
+    }
+    if stack.len() == 0 {
+        LineState::Complete
+    } else {
+        stack.reverse();
+        let missing_chars = stack.iter().map(|c| PAIRS[&c]).collect();
+        LineState::Incomplete(missing_chars)
+    }
+}
+
+fn corruption_score(lines: &Vec<String>) -> u32 {
+    lines
+        .iter()
+        .map(process_line)
+        .map(|status| match status {
+            LineState::Corrupted(c) => CORRUPTION_SCORES[&c],
+            _ => 0,
+        })
+        .sum()
+}
+
+fn line_score(state: LineState) -> u64 {
+    let mut score = 0;
+    if let LineState::Incomplete(missing_chars) = state {
+        for c in missing_chars {
+            score *= 5;
+            score += COMPLETION_SCORES[&c];
+        }
+        score
+    } else {
+        panic!("line_score only implemented for Incomplete")
+    }
+}
+
+fn completion_score(lines: &Vec<String>) -> u64 {
+    let mut scores: Vec<_> = lines
+        .iter()
+        .map(process_line)
+        .filter(|status| match status {
+            LineState::Incomplete(_) => true,
+            _ => false,
+        })
+        .map(line_score)
+        .collect();
+    scores.sort();
+    scores[(scores.len() - 1) / 2]
+}
+
+fn main() {
+    let input = utils::input_lines(10);
+    let part_1_answer = corruption_score(&input);
+    println!("Day 10, Part 1 answer: {}", part_1_answer);
+    let part_2_answer = completion_score(&input);
+    println!("Day 10, Part 2 answer: {}", part_2_answer);
+}
+
+#[cfg(test)]
+mod test;
---
+++ AOC2021/day10/src/test.rs
@@ -0,0 +1,82 @@
+use super::*;
+use rstest::{fixture, rstest};
+
+const TEST_1: &str = "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]";
+
+#[fixture]
+fn input() -> Vec<String> {
+    utils::string_input_lines(TEST_1)
+}
+
+#[rstest]
+fn test_n_corrupted(input: Vec<String>) {
+    assert_eq!(
+        input
+            .iter()
+            .map(process_line)
+            .filter(|status| match status {
+                LineState::Corrupted(_) => true,
+                _ => false,
+            })
+            .collect::<Vec<_>>()
+            .len(),
+        5
+    )
+}
+
+#[rstest]
+fn test_n_incomplete(input: Vec<String>) {
+    assert_eq!(
+        input
+            .iter()
+            .map(process_line)
+            .filter(|status| match status {
+                LineState::Incomplete(_) => true,
+                _ => false,
+            })
+            .collect::<Vec<_>>()
+            .len(),
+        5
+    )
+}
+
+#[rstest]
+fn test_incomplete_scores(input: Vec<String>) {
+    let scores = input
+        .iter()
+        .map(process_line)
+        .filter(|status| match status {
+            LineState::Incomplete(_) => true,
+            _ => false,
+        })
+        .map(line_score)
+        .collect::<Vec<_>>();
+    assert_eq!(scores, vec![288957, 5566, 1480781, 995444, 294])
+}
+
+#[rstest]
+fn test_incomplete_score_total(input: Vec<String>) {
+    assert_eq!(completion_score(&input), 288957);
+}
+
+#[rstest]
+fn test_corruption_score(input: Vec<String>) {
+    assert_eq!(corruption_score(&input), 26397);
+}
+
+#[test]
+fn test_part1() {
+    let input = utils::input_lines(10);
+
+    let part_1_answer = corruption_score(&input);
+    assert_eq!(part_1_answer, 166191);
+}
