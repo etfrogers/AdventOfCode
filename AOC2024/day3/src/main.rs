98a09e4e071de67cc6bfc0ecbc72a7b8ab0702ea
Ed Rogers <etfrogers@hotmail.com>
2024-12-06 22:15:37 +0000

    Day 3 Part 2

--- AOC2024/day3/src/main.rs
+++ AOC2024/day3/src/main.rs
@@ -1,26 +1,45 @@
 use std::sync::LazyLock;
 
-use regex::Regex;
+use fancy_regex::Regex;
 use utils;
 
 static MUL_RE: LazyLock<Regex> =
     LazyLock::new(|| Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap());
 
-fn process_muls(input: Vec<String>) -> u64 {
-    let input = &input.join(" ");
+static COND_RE: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"(^|do(?!(n't)))(.*?)(don't|$)").unwrap());
+
+fn process_muls(input: &Vec<String>) -> u64 {
+    mul_sum(&input.join(" "))
+}
+
+fn conditional_muls(input: &Vec<String>) -> u64 {
+    COND_RE
+        .captures_iter(&input.join(" "))
+        .map(|c| {
+            let c = c.unwrap();
+            // println!("{:?}\n---\n{}\n\n", c, c[0].to_string());
+            mul_sum(&c[3])
+        })
+        .sum()
+}
+
+fn mul_sum(s: &str) -> u64 {
     MUL_RE
-        .captures_iter(input)
+        .captures_iter(s)
         .map(|m| {
-            m.get(1).unwrap().as_str().parse::<u64>().unwrap()
-                * m.get(2).unwrap().as_str().parse::<u64>().unwrap()
+            let m = m.unwrap();
+            m[1].parse::<u64>().unwrap() * m[2].parse::<u64>().unwrap()
         })
         .sum()
 }
 
 fn main() {
     let input = utils::input_lines(3);
-    let part_1_answer = process_muls(input);
+    let part_1_answer = process_muls(&input);
     println!("Day 3, Part 1 answer: {}", part_1_answer);
+    let part_2_answer = conditional_muls(&input);
+    println!("Day 3, Part 2 answer: {}", part_2_answer);
 }
 
 #[cfg(test)]
--- AOC2024/day3/src/test.rs
+++ AOC2024/day3/src/test.rs
@@ -2,6 +2,7 @@ use super::*;
 use rstest::{fixture, rstest};
 
 const TEST_1: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+const TEST_2: &str = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
 #[fixture]
 fn input() -> Vec<String> {
@@ -10,13 +11,27 @@ fn input() -> Vec<String> {
 
 #[rstest]
 fn test_mul_finding(input: Vec<String>) {
-    assert_eq!(process_muls(input), 161);
+    assert_eq!(process_muls(&input), 161);
+}
+
+#[rstest]
+fn test_conditional(input: Vec<String>) {
+    assert_eq!(conditional_muls(&input), 161);
+    assert_eq!(conditional_muls(&vec![TEST_2.to_string()]), 48);
 }
 
 #[test]
 fn test_part1() {
     let input = utils::input_lines(3);
 
-    let part_1_answer = process_muls(input);
+    let part_1_answer = process_muls(&input);
     assert_eq!(part_1_answer, 192767529);
+}
+
+#[test]
+fn test_part2() {
+    let input = utils::input_lines(3);
+
+    let part_2_answer = conditional_muls(&input);
+    assert_eq!(part_2_answer, 104083373);
 }
