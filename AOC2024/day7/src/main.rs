a324701f481d1e71ca194fa325cc530bfd707091
Ed Rogers <etfrogers@hotmail.com>
2024-12-16 21:31:32 +0000

    Add coverage script

--- AOC2024/day7/src/main.rs
+++ AOC2024/day7/src/main.rs
@@ -1,6 +1,5 @@
 use itertools::{repeat_n, Itertools};
 use std::{num::ParseIntError, str::FromStr};
-use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
 use utils::{self, StringParseError};
 
@@ -15,6 +14,7 @@ struct Calibration(Vec<Equation>);
 enum Operator {
     Multiply,
     Add,
+    Concatenate,
 }
 
 impl FromStr for Equation {
@@ -33,10 +33,14 @@ impl FromStr for Equation {
 }
 
 impl Equation {
-    fn can_be_valid(&self) -> bool {
+    fn can_be_valid(&self, with_concat: bool) -> bool {
         let n_ops = self.inputs.len() - 1;
         // println!("");
-        for op_set in repeat_n(Operator::iter(), n_ops).multi_cartesian_product() {
+        let mut all_ops = vec![Operator::Add, Operator::Multiply];
+        if with_concat {
+            all_ops.push(Operator::Concatenate)
+        }
+        for op_set in repeat_n(all_ops, n_ops).multi_cartesian_product() {
             // println!("{:?}", op_set);
             if self.apply_ops(op_set) == self.target {
                 return true;
@@ -47,11 +51,20 @@ impl Equation {
 
     fn apply_ops(&self, ops: Vec<Operator>) -> u64 {
         let mut result = self.inputs[0];
+
         for (op, val) in ops.iter().zip(self.inputs[1..].iter()) {
             result = match op {
                 Operator::Add => result + val,
                 Operator::Multiply => result * val,
+                Operator::Concatenate => {
+                    let mut s = result.to_string();
+                    s.push_str(&val.to_string());
+                    s.parse().unwrap()
+                }
             };
+            if result > self.target {
+                return 0;
+            }
         }
         result
     }
@@ -68,14 +81,14 @@ impl Calibration {
         )
     }
 
-    fn _validity(&self) -> Vec<bool> {
-        self.0.iter().map(|v| v.can_be_valid()).collect()
+    fn _validity(&self, with_concat: bool) -> Vec<bool> {
+        self.0.iter().map(|v| v.can_be_valid(with_concat)).collect()
     }
 
-    fn total_calibration_result(&self) -> u64 {
+    fn total_calibration_result(&self, with_concat: bool) -> u64 {
         self.0
             .iter()
-            .filter(|e| e.can_be_valid())
+            .filter(|e| e.can_be_valid(with_concat))
             .map(|e| e.target)
             .sum()
     }
@@ -84,8 +97,11 @@ impl Calibration {
 fn main() {
     let input = utils::input_lines(7);
     let cal = Calibration::new(input);
-    let part_1_answer = cal.total_calibration_result();
+    let part_1_answer = cal.total_calibration_result(false);
     println!("Day 7, Part 1 answer: {}", part_1_answer);
+
+    let part_2_answer = cal.total_calibration_result(true);
+    println!("Day 7, Part 2 answer: {}", part_2_answer);
 }
 
 #[cfg(test)]
--- AOC2024/day7/src/test.rs
+++ AOC2024/day7/src/test.rs
@@ -20,21 +20,35 @@ fn input() -> Vec<String> {
 fn test_validity(input: Vec<String>) {
     let cal = Calibration::new(input);
     assert_eq!(
-        cal._validity(),
+        cal._validity(false),
         vec![true, true, false, false, false, false, false, false, true]
-    )
+    );
+
+    assert_eq!(
+        cal._validity(true),
+        vec![true, true, false, true, true, false, true, false, true]
+    );
 }
 
 #[rstest]
 fn test_total(input: Vec<String>) {
     let cal = Calibration::new(input);
-    assert_eq!(cal.total_calibration_result(), 3749)
+    assert_eq!(cal.total_calibration_result(false), 3749);
+    assert_eq!(cal.total_calibration_result(true), 11387);
 }
 
 #[test]
 fn test_part1() {
     let input = utils::input_lines(7);
     let cal = Calibration::new(input);
-    let part_1_answer = cal.total_calibration_result();
+    let part_1_answer = cal.total_calibration_result(false);
     assert_eq!(part_1_answer, 2501605301465);
+}
+
+#[test]
+fn test_part2() {
+    let input = utils::input_lines(7);
+    let cal = Calibration::new(input);
+    let part_1_answer = cal.total_calibration_result(true);
+    assert_eq!(part_1_answer, 44841372855953);
 }
