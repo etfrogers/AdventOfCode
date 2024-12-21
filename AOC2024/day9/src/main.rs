73d08981313a82cf2afeea8e436aaddfa70b8528
Ed Rogers <etfrogers@hotmail.com>
2024-12-21 22:22:10 +0000

    Day 9 Part 2

--- AOC2024/day9/src/main.rs
+++ AOC2024/day9/src/main.rs
@@ -2,55 +2,100 @@ use std::fmt::Display;
 
 use itertools::Itertools;
 
-use utils;
-
 struct DiskMap(Vec<Option<u32>>);
-struct Defragged(Vec<u32>);
+struct Gap {
+    start: usize,
+    len: usize,
+}
 
 impl DiskMap {
-    fn new(input: &Vec<String>) -> Self {
+    fn new(input: &[String]) -> Self {
         assert_eq!(input.len(), 1);
         let input = &input[0];
         let mut map = Vec::new();
-        let mut id = 0;
-        for mut sizes in &input.chars().chunks(2) {
+        for (id, mut sizes) in input.chars().chunks(2).into_iter().enumerate() {
             let file_size: usize = sizes.next().unwrap().to_string().parse().unwrap();
             let blank_size: usize = sizes.next().unwrap_or('0').to_string().parse().unwrap();
-            map.extend(vec![Some(id); file_size]);
+            map.extend(vec![Some(id.try_into().unwrap()); file_size]);
             map.extend(vec![None; blank_size]);
-            id += 1;
         }
         Self(map)
     }
 
-    fn defrag(&self) -> Defragged {
+    fn defrag_blocks(&self) -> DiskMap {
         let mut n_file_blocks = 0;
         self.0.iter().for_each(|v| {
             if v.is_some() {
                 n_file_blocks += 1
             }
         });
-        let mut files = self.0.clone();
-        let mut defragged = Vec::new();
-        defragged.resize(n_file_blocks, 0);
-        for (i, block) in defragged.iter_mut().enumerate() {
-            *block = files[i].unwrap_or_else(|| loop {
-                match files.pop().unwrap() {
-                    Some(id) => break id,
-                    None => (),
+        let mut defragged = self.0.clone();
+        let mut rep_index = defragged.len() - 1;
+        for i in 0..n_file_blocks {
+            let block = defragged.get(i).unwrap();
+            if block.is_none() {
+                while defragged[rep_index].is_none() {
+                    rep_index -= 1;
                 }
-            })
+                defragged.swap(i, rep_index);
+            }
+            // println!("{}", Self(defragged.clone()));
         }
-        Defragged(defragged)
+        defragged.resize(n_file_blocks, None);
+        Self(defragged)
     }
-}
 
-impl Defragged {
+    fn defrag_files(&self) -> DiskMap {
+        let mut defragged = self.0.clone();
+        let max_id = defragged
+            .iter()
+            .map(|v| v.unwrap_or_default())
+            .max()
+            .unwrap();
+
+        let mut gaps: Vec<Gap> = Vec::new();
+        let mut start = 0;
+        while start < defragged.len() {
+            if defragged[start].is_none() {
+                let mut len = 0;
+                while defragged[start + len + 1].is_none() {
+                    len += 1;
+                }
+                len += 1;
+                gaps.push(Gap { start, len });
+                start += len;
+            } else {
+                start += 1;
+            }
+        }
+
+        for id in (0..=max_id).rev() {
+            let file_inds: Vec<_> = defragged
+                .iter()
+                .enumerate()
+                .filter(|(_, v)| **v == Some(id))
+                .map(|(i, _)| i)
+                .collect();
+            let file_size = file_inds.len();
+            if let Some(gap) = gaps.iter_mut().find(|g| g.len >= file_size) {
+                if gap.start <= file_inds[0] {
+                    for i in 0..file_size {
+                        defragged.swap(gap.start + i, file_inds[0] + i);
+                    }
+                    gap.start += file_size;
+                    gap.len -= file_size;
+                }
+            }
+            // println!("{}", Self(defragged.clone()))
+        }
+        Self(defragged)
+    }
+
     fn checksum(&self) -> u64 {
         self.0
             .iter()
             .enumerate()
-            .map(|(i, v)| i as u64 * (*v) as u64)
+            .map(|(i, v)| i as u64 * v.unwrap_or_default() as u64)
             .sum()
     }
 }
@@ -71,17 +116,13 @@ impl Display for DiskMap {
     }
 }
 
-impl Display for Defragged {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0.iter().map(|v| v.to_string()).join(""))
-    }
-}
-
 fn main() {
     let input = utils::input_lines(9);
     let dm = DiskMap::new(&input);
-    let part_1_answer = dm.defrag().checksum();
+    let part_1_answer = dm.defrag_blocks().checksum();
     println!("Day 9, Part 1 answer: {}", part_1_answer);
+    let part_2_answer = dm.defrag_files().checksum();
+    println!("Day 9, Part 2 answer: {}", part_2_answer);
 }
 
 #[cfg(test)]
--- AOC2024/day9/src/test.rs
+++ AOC2024/day9/src/test.rs
@@ -17,19 +17,41 @@ fn test_parse(input: Vec<String>) {
 #[rstest]
 fn test_defrag(input: Vec<String>) {
     let dm = DiskMap::new(&input);
-    assert_eq!(dm.defrag().to_string(), "0099811188827773336446555566")
+    assert_eq!(
+        dm.defrag_blocks().to_string(),
+        "0099811188827773336446555566"
+    )
 }
 
 #[rstest]
 fn test_checksum(input: Vec<String>) {
     let dm = DiskMap::new(&input);
-    assert_eq!(dm.defrag().checksum(), 1928)
+    assert_eq!(dm.defrag_blocks().checksum(), 1928)
+}
+
+#[rstest]
+#[case(TEST_1, "00992111777.44.333....5555.6666.....8888..", 2858)]
+#[case("1313165", "021......33333......", 169)]
+
+fn test_files(#[case] input: &str, #[case] exp_str: &str, #[case] exp_checksum: u64) {
+    let dm = DiskMap::new(&vec![input.to_owned()]);
+    let defrag = dm.defrag_files();
+    assert_eq!(defrag.to_string(), exp_str);
+    assert_eq!(defrag.checksum(), exp_checksum)
 }
 
 #[test]
 fn test_part1() {
     let input = utils::input_lines(9);
+    let dm = DiskMap::new(&input);
+    let part_1_answer = dm.defrag_blocks().checksum();
+    assert_eq!(part_1_answer, 6331212425418);
+}
 
-    let part_1_answer = 0;
-    assert_eq!(part_1_answer, 1);
+#[test]
+fn test_part2() {
+    let input = utils::input_lines(9);
+    let dm = DiskMap::new(&input);
+    let part_2_answer = dm.defrag_files().checksum();
+    assert_eq!(part_2_answer, 6363268339304);
 }
