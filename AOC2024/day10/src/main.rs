cfcd88e90e3b87357e8785036f730e2d9a0fe1ae
Ed Rogers <etfrogers@hotmail.com>
2024-12-22 21:30:10 +0000

    Day 10 Part 2

--- AOC2024/day10/src/main.rs
+++ AOC2024/day10/src/main.rs
@@ -8,6 +8,11 @@ struct TopoMap {
     data: Grid<u8>,
 }
 
+enum RatingType {
+    DistinctRoute,
+    EndPoint,
+}
+
 impl TopoMap {
     fn new(input: Vec<String>) -> Self {
         Self {
@@ -19,8 +24,8 @@ impl TopoMap {
         self.data.find_all(0)
     }
 
-    fn n_routes_from(&self, trailhead: Coord) -> usize {
-        let mut ends: FxHashSet<Coord> = FxHashSet::default();
+    fn trailhead_score(&self, trailhead: Coord, rating_type: RatingType) -> usize {
+        let mut ends = Vec::<Coord>::new();
         let mut stack = Vec::<Coord>::new();
         let mut visited: FxHashSet<Coord> = FxHashSet::default();
         stack.push(trailhead);
@@ -28,7 +33,7 @@ impl TopoMap {
             visited.insert(curr);
             let curr_val = self.data[curr];
             if curr_val == 9 {
-                ends.insert(curr);
+                ends.push(curr);
                 continue;
             } else {
                 for n in self.data.neighbour_coords(&curr, false) {
@@ -38,19 +43,33 @@ impl TopoMap {
                 }
             }
         }
-        ends.len()
+        match rating_type {
+            RatingType::EndPoint => FxHashSet::from_iter(ends.iter()).len(),
+            RatingType::DistinctRoute => ends.len(),
+        }
     }
 
-    fn total_routes(&self) -> usize {
-        self.trailheads().map(|th| self.n_routes_from(th)).sum()
+    fn total_score(&self) -> usize {
+        self.trailheads()
+            .map(|th| self.trailhead_score(th, RatingType::EndPoint))
+            .sum()
+    }
+
+    fn total_rating(&self) -> usize {
+        self.trailheads()
+            .map(|th| self.trailhead_score(th, RatingType::DistinctRoute))
+            .sum()
     }
 }
 
 fn main() {
     let input = utils::input_lines(10);
     let tm = TopoMap::new(input);
-    let part_1_answer = tm.total_routes();
+    let part_1_answer = tm.total_score();
     println!("Day 10, Part 1 answer: {}", part_1_answer);
+
+    let part_2_answer = tm.total_rating();
+    println!("Day 10, Part 2 answer: {}", part_2_answer);
 }
 
 #[cfg(test)]
--- AOC2024/day10/src/test.rs
+++ AOC2024/day10/src/test.rs
@@ -16,10 +16,17 @@ fn input() -> Vec<String> {
 }
 
 #[rstest]
-fn test_n_routes(input: Vec<String>) {
+fn test_total_score(input: Vec<String>) {
     let tm = TopoMap::new(input);
-    let n = tm.total_routes();
+    let n = tm.total_score();
     assert_eq!(n, 36)
+}
+
+#[rstest]
+fn test_total_rating(input: Vec<String>) {
+    let tm = TopoMap::new(input);
+    let n = tm.total_rating();
+    assert_eq!(n, 81)
 }
 
 #[test]
@@ -27,6 +34,15 @@ fn test_part1() {
     let input = utils::input_lines(10);
     let tm = TopoMap::new(input);
 
-    let part_1_answer = tm.total_routes();
+    let part_1_answer = tm.total_score();
     assert_eq!(part_1_answer, 698);
+}
+
+#[test]
+fn test_part2() {
+    let input = utils::input_lines(10);
+    let tm = TopoMap::new(input);
+
+    let part_2_answer = tm.total_rating();
+    assert_eq!(part_2_answer, 1436);
 }
